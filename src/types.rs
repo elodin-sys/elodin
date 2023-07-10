4e9e93049fedd8dad6eaea79e197f3ee71face66
Sascha Wise <me@saschawise.com>
2023-07-09 22:53:03 -0700

    feat: add torque to `SixDof`

--- src/types.rs
+++ src/types.rs
@@ -3,6 +3,8 @@ use nalgebra::Vector3;
 #[derive(Debug, Clone, Copy, PartialEq, PartialOrd)]
 pub struct Force(pub Vector3<f64>);
 #[derive(Debug, Clone, Copy, PartialEq, PartialOrd)]
+pub struct Torque(pub Vector3<f64>);
+#[derive(Debug, Clone, Copy, PartialEq, PartialOrd)]
 pub struct Mass(pub f64);
 #[derive(Debug, Clone, Copy, PartialEq, PartialOrd)]
 pub struct Pos(pub Vector3<f64>);
