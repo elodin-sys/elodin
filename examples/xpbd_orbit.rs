bafd1973c2180e91b27cebb218e6d6d39739471d
Sascha Wise <me@saschawise.com>
2023-07-19 21:44:02 -0700

    feat: add sensor support

--- examples/xpbd_orbit.rs
+++ examples/xpbd_orbit.rs
@@ -5,44 +5,45 @@ use paracosm::{
     xpbd::{Entity, Xpbd},
     *,
 };
-use std::cell::RefCell;
-use std::rc::Rc;
+
+const DT: f64 = 0.001;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let rec_stream = rerun::RecordingStreamBuilder::new("proso_orbit").save("./out.rrd")?;
-    let mut xpbd = Xpbd::default();
-    let points = Rc::new(RefCell::new(vec![]));
-    let entity_points = points.clone();
-    xpbd.entity(
-        Entity::builder()
-            .mass(1.0)
-            .pos(vector![1.0, 0.0, 0.0])
-            .vel(vector![0.0, 1.0, 0.0])
-            .effector(gravity(1.0 / 6.649e-11, Vector3::zeros()))
-            .effector(|Time(t)| {
-                if (9.42..10.0).contains(&t) {
-                    Force(Vector3::new(0., 0.4, 0.5))
-                } else {
-                    Force(Vector3::zeros())
-                }
-            })
-            .sensor(sensor::rerun::time_pos_sensor(rec_stream.clone()))
-            .sensor(sensor::rerun::vel_sensor(rec_stream.clone()))
-            .sensor(sensor::rerun::pos_sensor(rec_stream.clone()))
-            .sensor(move |Pos(p)| {
-                entity_points
-                    .borrow_mut()
-                    .push(rerun::components::Point3D::new(
+    let rec_stream = rerun::RecordingStreamBuilder::new("paracosm_orbit").save("./out.rrd")?;
+    let mut points = vec![];
+
+    {
+        let mut xpbd = Xpbd::default();
+        xpbd.entity(
+            Entity::builder()
+                .mass(1.0)
+                .pos(vector![1.0, 0.0, 0.0])
+                .vel(vector![0.0, 1.0, 0.0])
+                .effector(gravity(1.0 / 6.649e-11, Vector3::zeros()))
+                .effector(|Time(t)| {
+                    if (9.42..10.0).contains(&t) {
+                        Force(Vector3::new(0., 0.4, 0.5))
+                    } else if (12.0..13.0).contains(&t) {
+                        Force(Vector3::new(0.2, 0.0, 0.5))
+                    } else {
+                        Force(Vector3::zeros())
+                    }
+                })
+                .sensor(sensor::rerun::time_pos_sensor(rec_stream.clone()))
+                .sensor(sensor::rerun::vel_sensor(rec_stream.clone()))
+                .sensor(sensor::rerun::pos_sensor(rec_stream.clone()))
+                .sensor(|Pos(p)| {
+                    points.push(rerun::components::Point3D::new(
                         p.x as f32, p.y as f32, p.z as f32,
                     ));
-            }),
-    );
+                }),
+        );
 
-    let mut time = 0;
-    let dt = 0.001;
-    while time <= 60_000 {
-        xpbd.tick(dt);
-        time += 1;
+        let mut time = 0;
+        while time <= 60_000 {
+            xpbd.tick(DT);
+            time += 1;
+        }
     }
 
     rerun::MsgSender::new("central_body")
@@ -52,8 +53,9 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         .send(&rec_stream)?;
 
     rerun::MsgSender::new("total_pos")
-        .with_component(&points.borrow()[..])?
+        .with_component(&points[..])?
         .with_timeless(true)
         .send(&rec_stream)?;
+
     Ok(())
 }
