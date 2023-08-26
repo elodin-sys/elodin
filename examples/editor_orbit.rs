b04fbdb46719b2c346fb55c1bfb3733be322b4a5
Sascha Wise <me@saschawise.com>
2023-08-26 07:51:41 -0700

    feat(xpbd): refactor builder to return entity ids

--- examples/editor_orbit.rs
+++ examples/editor_orbit.rs
@@ -6,36 +6,35 @@ use paracosm::{
         builder::{Assets, EntityBuilder, XpbdBuilder},
         editor::{editor, Input},
     },
-    Force, Time,
+    Force, Pos,
 };
 
 fn main() {
     editor(sim)
 }
 
-fn sim(mut assets: Assets, input: Input) -> XpbdBuilder {
-    XpbdBuilder::default()
-        .entity(
-            EntityBuilder::default()
-                .mass(1.0)
-                .pos(vector![0.0, 0.0, 1.0])
-                .vel(vector![1.0, 0.0, 0.0])
-                .effector(gravity(1.0 / 6.649e-11, Vector3::zeros()))
-                .effector(move |Time(_)| Force(Vector3::new(0.0, 0.0, *input.0.load())))
-                .mesh(assets.mesh(Mesh::from(shape::UVSphere {
-                    radius: 0.1,
-                    ..Default::default()
-                })))
-                .material(assets.material(Color::rgb(1.0, 0.0, 0.0).into())),
-        )
-        .entity(
-            EntityBuilder::default()
-                .mass(1.0)
-                .pos(Vector3::zeros())
-                .mesh(assets.mesh(Mesh::from(shape::UVSphere {
-                    radius: 0.1,
-                    ..Default::default()
-                })))
-                .material(assets.material(Color::rgb(0.0, 0.2, 1.0).into())),
-        )
+fn sim(mut builder: XpbdBuilder<'_>, mut assets: Assets, input: Input) {
+    builder.entity(
+        EntityBuilder::default()
+            .mass(1.0)
+            .pos(vector![0.0, 0.0, 1.0])
+            .vel(vector![1.0, 0.0, 0.0])
+            .effector(gravity(1.0 / 6.649e-11, Vector3::zeros()))
+            .effector(move |Pos(pos)| Force(*input.0.load() * pos.normalize()))
+            .mesh(assets.mesh(Mesh::from(shape::UVSphere {
+                radius: 0.1,
+                ..Default::default()
+            })))
+            .material(assets.material(Color::rgb(1.0, 0.0, 0.0).into())),
+    );
+    builder.entity(
+        EntityBuilder::default()
+            .mass(1.0)
+            .pos(Vector3::zeros())
+            .mesh(assets.mesh(Mesh::from(shape::UVSphere {
+                radius: 0.1,
+                ..Default::default()
+            })))
+            .material(assets.material(Color::rgb(0.0, 0.2, 1.0).into())),
+    );
 }
