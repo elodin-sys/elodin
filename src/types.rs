1d5e4f0525cf6786e7e356deaca1434f9dff2db2
Sascha Wise <me@saschawise.com>
2023-09-01 11:34:49 -0700

    feat(xpbd): add input that detects change

--- src/types.rs
+++ src/types.rs
@@ -2,7 +2,7 @@ use std::{
     marker::PhantomData,
     ops::{Deref, DerefMut},
     sync::{
-        atomic::{AtomicU64, Ordering},
+        atomic::{AtomicBool, AtomicU64, Ordering},
         Arc,
     },
 };
@@ -105,5 +105,69 @@ impl ToU64Storage for f64 {
 
     fn to_bits(&self) -> u64 {
         f64::to_bits(*self)
+    }
+}
+
+#[derive(Clone, Debug)]
+pub struct ObservableNum<T> {
+    storage: Arc<ObservableNumInner>,
+    _phantom: PhantomData<T>,
+}
+#[derive(Debug)]
+struct ObservableNumInner {
+    num: AtomicU64,
+    changed: AtomicBool,
+}
+
+impl<T: ToU64Storage + Default> Default for ObservableNum<T> {
+    fn default() -> Self {
+        Self {
+            storage: Arc::new(ObservableNumInner {
+                num: AtomicU64::new(T::default().to_bits()),
+                changed: AtomicBool::new(false),
+            }),
+            _phantom: Default::default(),
+        }
+    }
+}
+
+impl<T: ToU64Storage> ObservableNum<T> {
+    pub fn load(&self) -> ObservableNumRef<'_, T> {
+        let storage = self.storage.num.load(Ordering::SeqCst);
+        ObservableNumRef {
+            storage: self.storage.as_ref(),
+            num: T::from_bits(storage),
+        }
+    }
+
+    pub fn has_changed(&self) -> bool {
+        self.storage.changed.swap(false, Ordering::SeqCst)
+    }
+}
+
+pub struct ObservableNumRef<'a, T: ToU64Storage> {
+    storage: &'a ObservableNumInner,
+    num: T,
+}
+
+impl<'a, T: ToU64Storage> Deref for ObservableNumRef<'a, T> {
+    type Target = T;
+
+    fn deref(&self) -> &Self::Target {
+        &self.num
+    }
+}
+
+impl<'a, T: ToU64Storage> DerefMut for ObservableNumRef<'a, T> {
+    fn deref_mut(&mut self) -> &mut Self::Target {
+        &mut self.num
+    }
+}
+
+impl<'a, T: ToU64Storage> Drop for ObservableNumRef<'a, T> {
+    fn drop(&mut self) {
+        let bits = self.num.to_bits();
+        let old = self.storage.num.swap(bits, Ordering::SeqCst);
+        self.storage.changed.store(old != bits, Ordering::SeqCst);
     }
 }
