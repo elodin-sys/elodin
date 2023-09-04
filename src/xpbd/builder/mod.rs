ffa5b5daa97cfbc6d9ab7273b461ef94f2dbff8e
Sascha Wise <me@saschawise.com>
2023-09-04 13:45:58 -0700

    feat(xpbd): create own module for builder

--- src/xpbd/builder/mod.rs
+++ src/xpbd/builder/mod.rs
@@ -243,3 +243,66 @@ impl<'a> Assets<'a> {
 }
 
 pub struct AssetHandle<T: Asset>(Option<Handle<T>>);
+
+pub trait FromEnv<E: Env> {
+    type Item<'a>
+    where
+        E: 'a;
+
+    fn init(env: &mut E);
+
+    fn from_env(env: <E as Env>::Param<'_>) -> Self::Item<'_>;
+}
+
+pub trait Env {
+    type Param<'a>: Clone
+    where
+        Self: 'a;
+
+    fn param(&mut self) -> Self::Param<'_>;
+}
+
+pub trait SimBuilder<T, E: Env> {
+    fn build(self, env: &mut E);
+}
+
+macro_rules! impl_sim_builder {
+     ($($ty:tt),+) => {
+         #[allow(non_snake_case)]
+         impl<F, $($ty,)* E> SimBuilder<($($ty, )*), E> for F
+         where
+             E: Env,
+             F: Fn($($ty, )*),
+             F: for<'a> Fn($(<$ty as FromEnv<E>>::Item<'a>, )*) ,
+             $($ty: FromEnv<E>, )*
+         {
+
+             fn build(self, env: &mut E)  {
+
+                 $(
+                         $ty::init(env);
+                 )*
+                 let param = env.param();
+                 $(
+                     let $ty = $ty::from_env(param.clone());
+                 )*
+                 let res = (self)($($ty,)*);
+                 drop(param);
+                 res
+             }
+         }
+     };
+ }
+
+impl_sim_builder!(T1);
+impl_sim_builder!(T1, T2);
+impl_sim_builder!(T1, T2, T3);
+impl_sim_builder!(T1, T2, T3, T4);
+impl_sim_builder!(T1, T2, T3, T4, T5);
+impl_sim_builder!(T1, T2, T3, T4, T5, T6);
+impl_sim_builder!(T1, T2, T3, T4, T5, T6, T7);
+impl_sim_builder!(T1, T2, T3, T4, T5, T6, T7, T8);
+impl_sim_builder!(T1, T2, T3, T4, T5, T6, T7, T9, T10);
+impl_sim_builder!(T1, T2, T3, T4, T5, T6, T7, T9, T10, T11);
+impl_sim_builder!(T1, T2, T3, T4, T5, T6, T7, T9, T10, T11, T12);
+impl_sim_builder!(T1, T2, T3, T4, T5, T6, T7, T9, T10, T11, T12, T13);
