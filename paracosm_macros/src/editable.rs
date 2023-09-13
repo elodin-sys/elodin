9a43b50b1e4a2c030ca982ab67e3511c3e3e6892
Andrei Khvalko <andrey.greams@gmail.com>
2023-09-13 19:19:09 +0200

    Add macros for paracosm (#16)
    * Add prcsm_macros crate
    
    * Fix most linter issues
    
    * Rename lib and update dependency format
    
    * Fix impl macro and remove logs
    
    * Use quotes for signed numbers

---
+++ paracosm_macros/src/editable.rs
@@ -0,0 +1,80 @@
+use darling::ast::NestedMeta;
+use darling::Error;
+use darling::FromMeta;
+use proc_macro::TokenStream;
+use quote::{quote, ToTokens};
+use syn::{parse_macro_input, punctuated::Punctuated, DeriveInput, Meta, Token};
+
+#[derive(Debug, FromMeta)]
+pub struct EditableAttributes {
+    #[darling(default)]
+    slider: bool,
+    #[darling(default)]
+    range_min: f64,
+    #[darling(default)]
+    range_max: f64,
+    name: String,
+}
+
+pub fn derive_proc_macro_impl(input: TokenStream) -> TokenStream {
+    let DeriveInput {
+        ident: item_identifier,
+        data: _,
+        generics,
+        attrs,
+        ..
+    } = parse_macro_input!(input as DeriveInput);
+
+    let where_clause = &generics.where_clause;
+
+    //--------------------------------------------------------------------------
+
+    let _attr = attrs.iter().find(|attr| attr.path().is_ident("editable"));
+
+    if let Some(attr) = _attr {
+        let nested = attr
+            .parse_args_with(Punctuated::<Meta, Token![,]>::parse_terminated)
+            .unwrap();
+
+        let attr_args = match NestedMeta::parse_meta_list(nested.to_token_stream()) {
+            Ok(v) => v,
+            Err(e) => {
+                return TokenStream::from(Error::from(e).write_errors());
+            }
+        };
+
+        let editable_attributes = match EditableAttributes::from_list(&attr_args) {
+            Ok(v) => v,
+            Err(e) => {
+                return TokenStream::from(e.write_errors());
+            }
+        };
+
+        let EditableAttributes {
+            range_min,
+            range_max,
+            name,
+            ..
+        } = editable_attributes;
+
+        quote! {
+            impl Editable for #item_identifier #generics #where_clause {
+                fn build(&mut self, ui: &mut Ui) {
+                    let mut num = self.0.load();
+                    ui.add(egui::Slider::new(num.deref_mut(), #range_min..=#range_max).text(#name));
+                }
+            }
+        }
+        .into()
+    } else {
+        quote! {
+            impl Editable for #item_identifier #generics #where_clause {
+                fn build(&mut self, ui: &mut Ui) {
+                    let mut num = self.0.load();
+                    ui.add(egui::Slider::new(num.deref_mut(), -1.25..=1.25).text("input"));
+                }
+            }
+        }
+        .into()
+    }
+}
