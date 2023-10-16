0ec1d12fc0761c0327bc3c4b36f5fafa6cfa0adc
Sascha Wise <me@saschawise.com>
2023-10-16 08:59:01 -0700

    fix(tree): fix transforms and subspace
    Signed-off-by: Sascha Wise <me@saschawise.com>

--- libs/paracosm-core/src/spatial/subspace.rs
+++ libs/paracosm-core/src/spatial/subspace.rs
@@ -1,16 +1,22 @@
 use nalgebra::{Matrix6, Vector3};
 use std::ops::Mul;
 
-use super::{SpatialForce, SpatialMotion};
+use super::{SpatialForce, SpatialMotion, Transpose};
 
 #[derive(Debug)]
 pub struct SpatialSubspace(pub Matrix6<f64>);
 
-impl Mul<SpatialForce> for SpatialSubspace {
+impl SpatialSubspace {
+    fn transpose(self) -> Transpose<Self> {
+        Transpose(self)
+    }
+}
+
+impl Mul<SpatialForce> for Transpose<SpatialSubspace> {
     type Output = SpatialForce;
 
     fn mul(self, rhs: SpatialForce) -> Self::Output {
-        let out = self.0.transpose() * rhs.vector();
+        let out = self.0 .0.transpose() * rhs.vector();
         let torque = Vector3::new(out[0], out[1], out[2]);
         let force = Vector3::new(out[3], out[4], out[5]);
         SpatialForce { force, torque }
