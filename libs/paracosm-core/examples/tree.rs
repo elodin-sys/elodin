bbccecb7956c5d2c69123c7402781a40e383f6ee
Sascha Wise <me@saschawise.com>
2023-10-23 22:41:17 -0700

    feat(tree): add sphere pendulum example
    Signed-off-by: Sascha Wise <me@saschawise.com>

--- libs/paracosm-core/examples/tree.rs
+++ libs/paracosm-core/examples/tree.rs
@@ -1,4 +1,7 @@
-use bevy::prelude::{shape, Color, Mesh};
+use bevy::prelude::{
+    shape::{self, UVSphere},
+    Color, Mesh,
+};
 use nalgebra::{vector, Vector3};
 use paracosm::{
     builder::{Assets, EntityBuilder, FixedJoint, Revolute, XpbdBuilder},
@@ -12,24 +15,73 @@ fn main() {
 }
 
 fn sim(mut builder: XpbdBuilder<'_>, mut assets: Assets) {
+    // let root = builder.entity(
+    //     EntityBuilder::default()
+    //         .mass(10.0)
+    //         //.fixed()
+    //         .mesh(assets.mesh(Mesh::from(shape::UVSphere {
+    //             radius: 0.1,
+    //             ..Default::default()
+    //         })))
+    //         .material(assets.material(Color::rgb(1.0, 0.0, 0.0).into()))
+    //         .body_pos(SpatialPos::linear(vector![0., 0.0, 0.0]))
+    //         .joint(FixedJoint),
+    // );
+    // let rod_a = builder.entity(
+    //     EntityBuilder::default()
+    //         .mass(1.0)
+    //         .body_pos(SpatialPos::linear(vector![0., -0.5, 0.0]))
+    //         .inertia(paracosm::Inertia::solid_box(0.2, 1.0, 0.2, 1.0))
+    //         .mesh(assets.mesh(Mesh::from(shape::Box::new(0.2, 1.0, 0.2))))
+    //         .material(assets.material(bevy::prelude::StandardMaterial {
+    //             base_color: Color::hex("38ACFF").unwrap(),
+    //             metallic: 0.6,
+    //             perceptual_roughness: 0.1,
+    //             ..Default::default()
+    //         }))
+    //         .parent(
+    //             root,
+    //             Revolute::new(Vector3::z_axis()).pos(-170f64.to_radians()),
+    //         ),
+    // );
+
+    // builder.entity(
+    //     EntityBuilder::default()
+    //         .mass(1.0)
+    //         .body_pos(SpatialPos::linear(vector![0., -0.5, 0.0]))
+    //         .trace(Vector3::new(0., -0.5, 0.))
+    //         .inertia(paracosm::Inertia::solid_box(0.2, 1.0, 0.2, 1.0))
+    //         .mesh(assets.mesh(Mesh::from(shape::Box::new(0.2, 1.0, 0.2))))
+    //         .material(assets.material(Color::hex("FF9838").unwrap().into()))
+    //         .parent(
+    //             rod_a,
+    //             Revolute::new(Vector3::z_axis())
+    //                 .anchor(vector![0., -0.5, 0.0])
+    //                 .pos(-70f64.to_radians()),
+    //         ),
+    // );
+
     let root = builder.entity(
         EntityBuilder::default()
             .mass(10.0)
-            //.fixed()
-            .mesh(assets.mesh(Mesh::from(shape::UVSphere {
+            .mesh(assets.mesh(Mesh::from(UVSphere {
                 radius: 0.1,
                 ..Default::default()
             })))
             .material(assets.material(Color::rgb(1.0, 0.0, 0.0).into()))
-            .body_pos(SpatialPos::linear(vector![0., 2.0, 0.0]))
+            .body_pos(SpatialPos::linear(vector![0., 0.0, 0.0]))
             .joint(FixedJoint),
     );
     let rod_a = builder.entity(
         EntityBuilder::default()
             .mass(1.0)
-            .body_pos(SpatialPos::linear(vector![0., -0.5, 0.0]))
-            //.inertia(paracosm::Inertia::solid_box(0.2, 1.0, 0.2, 1.0))
-            .mesh(assets.mesh(Mesh::from(shape::Box::new(0.2, 1.0, 0.2))))
+            .body_pos(SpatialPos::linear(vector![0., -1.0, 0.0]))
+            .inertia(paracosm::Inertia::sphere(0.2, 1.0))
+            .mesh(assets.mesh(Mesh::from(UVSphere {
+                radius: 0.2,
+                sectors: 16,
+                ..Default::default()
+            })))
             .material(assets.material(bevy::prelude::StandardMaterial {
                 base_color: Color::hex("38ACFF").unwrap(),
                 metallic: 0.6,
@@ -38,23 +90,25 @@ fn sim(mut builder: XpbdBuilder<'_>, mut assets: Assets) {
             }))
             .parent(
                 root,
-                Revolute::new(Vector3::z_axis()).pos(90f64.to_radians()),
+                Revolute::new(Vector3::z_axis()).pos(170f64.to_radians()),
             ),
     );
 
     builder.entity(
         EntityBuilder::default()
             .mass(1.0)
-            .body_pos(SpatialPos::linear(vector![0., -0.5, 0.0]))
-            .trace(Vector3::new(0., -0.5, 0.))
-            //.inertia(paracosm::Inertia::solid_box(0.2, 1.0, 0.2, 1.0))
-            .mesh(assets.mesh(Mesh::from(shape::Box::new(0.2, 1.0, 0.2))))
+            .body_pos(SpatialPos::linear(vector![0., -1.0, 0.0]))
+            .trace(Vector3::new(0., 0.0, 0.))
+            .inertia(paracosm::Inertia::sphere(0.2, 1.0))
+            .mesh(assets.mesh(Mesh::from(UVSphere {
+                radius: 0.2,
+                sectors: 16,
+                ..Default::default()
+            })))
             .material(assets.material(Color::hex("FF9838").unwrap().into()))
             .parent(
                 rod_a,
-                Revolute::new(Vector3::z_axis())
-                    .anchor(vector![0., -0.5, 0.0])
-                    .pos(0f64.to_radians()),
+                Revolute::new(Vector3::z_axis()).pos(0f64.to_radians()),
             ),
     );
 }
