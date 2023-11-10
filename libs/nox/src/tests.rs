2668edfe145ef4aa01c3fa531ed486c0e5c53389
Sascha Wise <me@saschawise.com>
2023-11-10 13:24:27 -0800

    feat(nox): add matrix slice

--- libs/nox/src/tests.rs
+++ libs/nox/src/tests.rs
@@ -1,4 +1,5 @@
 use nalgebra::matrix;
+use std::ops::Add;
 
 use super::*;
 
@@ -32,4 +33,25 @@ fn test_map() {
     };
     let out = exec.run(&client, matrix![1.0f32, 2.0, 3.0, 4.0]).unwrap();
     assert_eq!(out, matrix![2.0, 3.0, 4.0, 5.0])
+}
+
+#[test]
+fn test_fixed_slice() {
+    let client = Client::cpu().unwrap();
+    fn slice(mat: Matrix<f32, 1, 4>) -> Matrix<f32, 1, 1> {
+        mat.fixed_slice::<1, 1>(0, 2)
+    }
+    let comp = slice.build().unwrap();
+    let exec = match comp.compile(&client) {
+        Ok(exec) => exec,
+        Err(xla::Error::XlaError { msg, .. }) => {
+            println!("{}", msg);
+            panic!();
+        }
+        Err(e) => {
+            panic!("{:?}", e);
+        }
+    };
+    let out = exec.run(&client, matrix![1.0f32, 2.0, 3.0, 4.0]).unwrap();
+    assert_eq!(out, matrix![3.0])
 }
