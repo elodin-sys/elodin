69dc14363cbc7bcabb114676e3f4eb9dead0e3d4
Sascha Wise <me@saschawise.com>
2023-11-28 20:23:12 -0800

    feat(sandbox-runner): switch to grpc and tcp

---
+++ services/paracosm-web-runner/src/config.rs
@@ -0,0 +1,20 @@
+use config::{ConfigError, Environment, File};
+use serde::Deserialize;
+use std::net::SocketAddr;
+
+#[derive(Debug, Deserialize)]
+pub struct Config {
+    pub control_addr: SocketAddr,
+    pub sim_addr: SocketAddr,
+}
+
+impl Config {
+    pub fn new() -> Result<Self, ConfigError> {
+        config::Config::builder()
+            .add_source(File::with_name("./config.toml"))
+            .add_source(File::with_name("/etc/elodin/sim.toml"))
+            .add_source(Environment::with_prefix("ELODIN"))
+            .build()?
+            .try_deserialize()
+    }
+}
