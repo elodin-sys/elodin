e28720ffc4bee967665f940fbaae4d9c17a3784b
Andrei Khvalko <andrey.greams@gmail.com>
2023-12-17 02:24:39 +0100

    Replace buildkite auth plugin with manual process (#63)
    * Replace auth plugin with manual cmds
    
    * Fix google creds path
    
    * Enable image build/push with new auth
    
    * Re-arrange cmds for auth
    
    * Restructure build-image group
    
    * Enable tests

--- .buildkite/steps.py
+++ .buildkite/steps.py
@@ -4,33 +4,32 @@ from plugins import *
 def build_image_step(image_name, service_path, image_tag = "latest", repository = "us-central1-docker.pkg.dev"):
   remote_image_name = f"{repository}/elodin-dev/{image_name}/x86_64"
 
-  return group(
-    name = f":docker: {image_name}",
-    key = image_name,
-    steps = [
-      step(
-        label = f":docker: build {image_name}",
-        key = f"build-{image_name}",
-        command = [
-          f"cd {service_path}",
-          "nix flake update",
-          "nix build .#packages.x86_64-linux.docker.image",
-          "cat result | docker load",
-          f"docker tag {image_name}:latest {remote_image_name}:{image_tag}",
-        ],
-      ),
-      # NOTE: Workload auth is only active for about 10min so we need to trigger plugin when image is ready to be pushed
-      step(
-        label = f":docker: push {image_name}",
-        depends_on = f"build-{image_name}",
-        command = [
-          "gcloud --quiet auth login --cred-file=\$GOOGLE_APPLICATION_CREDENTIALS",
-          f"gcloud --quiet auth configure-docker {repository}",
-          f"docker push {remote_image_name}:{image_tag}",
-        ],
-        plugins = [ gcp_identity_plugin() ],
-      ),
-    ]
+  return step(
+    label = f":docker: {image_name}",
+    command = [
+      # NOTE: Prepare environment
+      "export GCP_WIF_TMPDIR=$(mktemp -d -t 'buildkiteXXXX')",
+      "export GOOGLE_APPLICATION_CREDENTIALS=\$GCP_WIF_TMPDIR/credentials.json",
+      "envsubst < .buildkite/credentials.template.json > \$GOOGLE_APPLICATION_CREDENTIALS",
+      # NOTE: Build image
+      f"cd {service_path}",
+      "nix flake update",
+      "nix build .#packages.x86_64-linux.docker.image",
+      "cat result | docker load",
+      f"docker tag {image_name}:latest {remote_image_name}:{image_tag}",
+      # NOTE: Login into GCP (active for 10min)
+      "buildkite-agent oidc request-token --audience \"\$GCP_WIF_AUDIENCE\" > \$GCP_WIF_TMPDIR/token.json",
+      "gcloud --quiet auth login --cred-file=\$GOOGLE_APPLICATION_CREDENTIALS",
+      # NOTE: Push image
+      f"gcloud --quiet auth configure-docker {repository}",
+      f"docker push {remote_image_name}:{image_tag}",
+      # NOTE: Clean up
+      "rm -rf \$GCP_WIF_TMPDIR",
+    ],
+    env = {
+      "GCP_WIF_AUDIENCE": "//iam.googleapis.com/projects/802981626435/locations/global/workloadIdentityPools/buildkite-pipeline/providers/buildkite",
+      "GCP_WIF_SERVICE_ACCOUNT": "buildkite-802981626435@elodin-dev.iam.gserviceaccount.com",
+    },
   )
 
 def nix_step(label, flake, command, emoji = ":nix:", env = {}, plugins = []):
