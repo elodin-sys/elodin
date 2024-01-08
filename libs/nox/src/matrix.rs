0b486e41c4cda51e4a45b0ea6aa33d22653e5de7
Sascha Wise <me@saschawise.com>
2024-01-07 21:02:37 -0800

    ELO-52: Add Noxpr and `vmap` implementations (#76)
    * feat(nox): move to new xla-rs version
    
    * feat(nox): replace xla ops with noxpr ir
    
    * feat(nox): add initial vmap impl
    
    * feat(nox): add remaining vmap batch impls
    
    * chore(nox): remove unused map tests

--- libs/nox/src/matrix.rs
+++ libs/nox/src/matrix.rs
@@ -1,9 +1,9 @@
 use crate::{
-    Buffer, BufferArg, Builder, Client, CompFn, FromHost, IntoOp, Literal, MaybeOwned, Op, Scalar,
-    Tensor, ToHost,
+    ArrayTy, Buffer, BufferArg, Client, FromHost, Literal, MaybeOwned, Noxpr, Op, Tensor, ToHost,
 };
 use nalgebra::{ArrayStorage, Const, IsContiguous, Scalar as NalgebraScalar, Storage};
 use num_traits::Zero;
+use smallvec::smallvec;
 use std::marker::PhantomData;
 use xla::{ArrayElement, NativeType};
 
@@ -18,7 +18,7 @@ where
     fn to_host(&self) -> Self::HostTy {
         let literal = self.inner.to_literal_sync().unwrap();
         let mut out = Self::HostTy::zeros();
-        literal.copy_raw_to(out.as_mut_slice()).unwrap();
+        out.copy_from_slice(literal.typed_buf().unwrap());
         out
     }
 }
@@ -35,12 +35,15 @@ where
     }
 }
 
-impl<T, const R: usize, const C: usize> Matrix<T, R, C, Literal> {
-    fn constant(self, builder: &Builder) -> Matrix<T, R, C, Op> {
-        let inner = builder
-            .inner
-            .constant_literal(&self.inner)
-            .expect("constant creation failed");
+impl<T: ArrayElement, const R: usize, const C: usize> Matrix<T, R, C, Literal> {
+    fn constant(self) -> Matrix<T, R, C, Op> {
+        let inner = Noxpr::constant(
+            self.inner,
+            ArrayTy {
+                element_type: T::TY,
+                shape: smallvec![R as i64, C as i64],
+            },
+        );
 
         Matrix {
             inner,
@@ -50,7 +53,7 @@ impl<T, const R: usize, const C: usize> Matrix<T, R, C, Literal> {
 }
 
 pub trait MatrixExt<T, const R: usize, const C: usize> {
-    fn constant(&self, builder: &Builder) -> Matrix<T, R, C, Op>;
+    fn constant(&self) -> Matrix<T, R, C, Op>;
     fn literal(&self) -> Matrix<T, R, C, Literal>;
     fn buffer(&self, client: &Client) -> Matrix<T, R, C, Buffer>;
 }
@@ -62,15 +65,12 @@ where
     S: Storage<T, Const<R>, Const<C>>,
     S: IsContiguous,
 {
-    fn constant(&self, builder: &Builder) -> Matrix<T, R, C, Op> {
-        self.literal().constant(builder)
+    fn constant(&self) -> Matrix<T, R, C, Op> {
+        self.literal().constant()
     }
 
     fn buffer(&self, client: &Client) -> Matrix<T, R, C, Buffer> {
-        let inner = client
-            .0
-            .buffer_from_host_buffer(self.as_slice(), &[R, C], None)
-            .unwrap();
+        let inner = client.0.copy_host_buffer(self.as_slice(), &[R, C]).unwrap();
         Matrix {
             inner,
             phantom: PhantomData,
@@ -78,24 +78,15 @@ where
     }
 
     fn literal(&self) -> Matrix<T, R, C, Literal> {
+        let inner = T::create_r1(self.as_slice())
+            .reshape(&[R as i64, C as i64])
+            .unwrap();
         Matrix {
-            inner: xla::Literal::vec1(self.as_slice())
-                .reshape(&[R as i64, C as i64])
-                .expect("reshape failed"),
+            inner,
             phantom: PhantomData,
         }
     }
 }
-
-// impl<T, const R: usize, const C: usize> FromPjrtBuffer
-//     for nalgebra::Matrix<T, Const<R>, Const<C>, ArrayStorage<T, R, C>>
-// where
-//     T: xla::NativeType + NalgebraScalar + Zero + ArrayElement,
-// {
-//     fn from_pjrt(pjrt: Vec<Vec<xla::PjRtBuffer>>) -> Self {
-//         let buf = &pjrt[0][0];
-//     }
-// }
 
 impl<T, const R: usize, const C: usize> FromHost for Matrix<T, R, C, Buffer>
 where
@@ -108,35 +99,62 @@ where
     }
 }
 
-pub trait MapExt {
-    type Elem;
-    type Output<R>;
+impl<T, const R: usize, const C: usize, const N: usize> FromHost
+    for Tensor<T, (Const<R>, Const<C>, Const<N>), Buffer>
+where
+    T: NativeType + NalgebraScalar + ArrayElement,
+{
+    type HostTy = [nalgebra::Matrix<T, Const<R>, Const<C>, ArrayStorage<T, R, C>>; N];
 
-    fn map<O>(
-        &self,
-        func: impl CompFn<(Self::Elem,), Scalar<O>>,
-    ) -> Result<Self::Output<O>, xla::Error>
-    where
-        Scalar<O>: IntoOp;
+    fn from_host(client: &Client, native: Self::HostTy) -> Self {
+        let mut buf = Vec::with_capacity(C * R * N);
+        for mat in native.iter() {
+            buf.extend_from_slice(mat.as_slice());
+        }
+        let inner = client.0.copy_host_buffer(&buf, &[N, R, C]).unwrap();
+        Tensor {
+            inner,
+            phantom: PhantomData,
+        }
+    }
 }
 
-impl<T: ArrayElement, const R: usize, const C: usize> MapExt for Matrix<T, R, C> {
-    type Elem = Scalar<T>;
+impl<T, S, const R: usize, const C: usize, const N: usize>
+    BufferArg<Tensor<T, (Const<R>, Const<C>, Const<N>), Buffer>>
+    for [nalgebra::Matrix<T, Const<R>, Const<C>, S>; N]
+where
+    T: xla::NativeType + NalgebraScalar + ArrayElement,
+    S: Storage<T, Const<R>, Const<C>>,
+    S: IsContiguous,
+{
+    fn as_buffer(&self, client: &Client) -> MaybeOwned<'_, xla::PjRtBuffer> {
+        let mut buf = Vec::with_capacity(C * R * N);
+        for mat in self.iter() {
+            buf.extend_from_slice(mat.as_slice());
+        }
+        let inner = client.0.copy_host_buffer(&buf, &[N, R, C]).unwrap();
+        MaybeOwned::Owned(inner)
+    }
+}
 
-    type Output<O> = Matrix<O, R, C>;
+pub trait Dot<Rhs = Self> {
+    type Output;
 
-    fn map<O>(
-        &self,
-        func: impl CompFn<(Self::Elem,), Scalar<O>>,
-    ) -> Result<Self::Output<O>, xla::Error>
-    where
-        Scalar<O>: IntoOp,
-    {
-        let comp = func.build()?;
-        Ok(Matrix {
-            inner: self.inner.map(comp.comp, &[0, 1])?,
+    fn dot(self, rhs: Rhs) -> Self::Output;
+}
+
+impl<T, const R: usize, const C: usize> Dot for Matrix<T, R, C, Op>
+where
+    T: NativeType + NalgebraScalar + ArrayElement,
+{
+    type Output = Matrix<T, R, C, Op>;
+
+    fn dot(self, rhs: Self) -> Self::Output {
+        let inner = Noxpr::dot(self.inner, &rhs.inner);
+        Matrix {
+            inner,
             phantom: PhantomData,
-        })
+        }
     }
 }
 
@@ -144,7 +162,7 @@ impl<T: ArrayElement, const R: usize, const C: usize> MapExt for Matrix<T, R, C>
 mod tests {
     use nalgebra::matrix;
 
-    use crate::FixedSliceExt;
+    use crate::{CompFn, FixedSliceExt};
 
     use super::*;
 
@@ -192,58 +210,6 @@ mod tests {
             .unwrap()
             .to_host();
         assert_eq!(out, matrix![2., 6.; 8., 15.]);
-    }
-
-    #[test]
-    fn test_map() {
-        let client = Client::cpu().unwrap();
-        fn add_one(mat: Matrix<f32, 1, 4>) -> Matrix<f32, 1, 4> {
-            mat.map(|x: Scalar<f32>| x + 1f32).unwrap()
-        }
-        let comp = add_one.build().unwrap();
-        let exec = match comp.compile(&client) {
-            Ok(exec) => exec,
-            Err(xla::Error::XlaError { msg, .. }) => {
-                println!("{}", msg);
-                panic!();
-            }
-            Err(e) => {
-                panic!("{:?}", e);
-            }
-        };
-        let out = exec
-            .run(&client, matrix![1.0f32, 2.0, 3.0, 4.0])
-            .unwrap()
-            .to_host();
-        assert_eq!(out, matrix![2.0, 3.0, 4.0, 5.0])
-    }
-
-    #[test]
-    fn test_map_mut() {
-        let client = Client::cpu().unwrap();
-        let comp = (|mat: &mut Matrix<f32, 1, 4>, b: &mut Matrix<f32, 1, 3>| {
-            *mat = mat.map(|x: Scalar<f32>| x + 1f32).unwrap();
-            *b = b.map(|x: Scalar<f32>| x + 2f32).unwrap();
-        })
-        .build()
-        .unwrap();
-        let exec = match comp.compile(&client) {
-            Ok(exec) => exec,
-            Err(xla::Error::XlaError { msg, .. }) => {
-                println!("{}", msg);
-                panic!();
-            }
-            Err(e) => {
-                panic!("{:?}", e);
-            }
-        };
-        let mut a = Matrix::from_host(&client, matrix![1.0f32, 2.0, 3.0, 4.0]);
-        let mut b = Matrix::from_host(&client, matrix![4.0f32, 0.0, 1.0]);
-        exec.run(&client, &mut a, &mut b).unwrap();
-        let a = a.to_host();
-        assert_eq!(a, matrix![2.0, 3.0, 4.0, 5.0]);
-        let b = b.to_host();
-        assert_eq!(b, matrix![6.0, 2.0, 3.0]);
     }
 
     #[test]
