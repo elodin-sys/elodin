a798f215762cd393b76d370362c0f0808489e8c2
Sascha Wise <me@saschawise.com>
2024-01-08 10:04:26 -0800

    ELO-51: Add initial `nox-ecs` implementation.  (#78)
    * feat(nox-ecs): add initial impl
    
    * feat(nox-ecs): port to noxprs
    
    * feat(nox-ecs): add basic query support
    
    * feat(nox-ecs): add rk4 integrator
    
    * feat(nox-ecs): add derive macros for component
    
    * chore(rust): bump `Cargo.lock` & `Cargo.nix`
    
    * feat(nox-ecs): add archetype macro
    
    * chore(nox-ecs): `NoxComponent` -> `Component`

--- libs/elodin-conduit/src/nox.rs
+++ libs/elodin-conduit/src/nox.rs
@@ -3,7 +3,7 @@ use nox::{
     Client,
 };
 
-use crate::ComponentValue;
+use crate::{ComponentType, ComponentValue};
 
 impl ComponentValue<'_> {
     pub fn to_pjrt_buf(&self, client: &Client) -> Result<PjRtBuffer, nox::Error> {
@@ -38,37 +38,79 @@ impl ComponentValue<'_> {
             }
             ComponentValue::Filter(_) => heapless::Vec::from_slice(&[9]).unwrap(),
         };
-        let element_ty = match self {
-            ComponentValue::U8(_) => ElementType::U8,
-            ComponentValue::U16(_) => ElementType::U16,
-            ComponentValue::U32(_) => ElementType::U32,
-            ComponentValue::U64(_) => ElementType::U64,
-            ComponentValue::I8(_) => ElementType::S8,
-            ComponentValue::I16(_) => ElementType::S16,
-            ComponentValue::I32(_) => ElementType::S32,
-            ComponentValue::I64(_) => ElementType::S64,
-            ComponentValue::Bool(_) => ElementType::U8,
-            ComponentValue::F32(_) => ElementType::F32,
-            ComponentValue::F64(_) => ElementType::F64,
-            ComponentValue::String(_) => ElementType::U8,
-            ComponentValue::Bytes(_) => ElementType::U8,
-            ComponentValue::Vector3F32(_)
-            | ComponentValue::Matrix3x3F32(_)
-            | ComponentValue::SpatialPosF32(_)
-            | ComponentValue::SpatialMotionF32(_)
-            | ComponentValue::QuaternionF32(_) => ElementType::F32,
-            ComponentValue::Vector3F64(_)
-            | ComponentValue::Matrix3x3F64(_)
-            | ComponentValue::QuaternionF64(_)
-            | ComponentValue::SpatialPosF64(_)
-            | ComponentValue::SpatialMotionF64(_) => ElementType::F64,
-            ComponentValue::Filter(_) => ElementType::U8,
-        };
+        let element_ty = self.ty().element_type();
         self.with_bytes(|bytes| {
             client
                 .0
                 .copy_raw_host_buffer(element_ty, bytes, &dims)
                 .map_err(nox::Error::from)
         })
+    }
+}
+
+impl ComponentType {
+    #[inline]
+    pub fn element_type(self) -> ElementType {
+        match self {
+            ComponentType::U8 => ElementType::U8,
+            ComponentType::U16 => ElementType::U16,
+            ComponentType::U32 => ElementType::U32,
+            ComponentType::U64 => ElementType::U64,
+            ComponentType::I8 => ElementType::S8,
+            ComponentType::I16 => ElementType::S16,
+            ComponentType::I32 => ElementType::S32,
+            ComponentType::I64 => ElementType::S64,
+            ComponentType::Bool => ElementType::U8,
+            ComponentType::F32 => ElementType::F32,
+            ComponentType::F64 => ElementType::F64,
+            ComponentType::String => ElementType::U8,
+            ComponentType::Bytes => ElementType::U8,
+            ComponentType::Vector3F32
+            | ComponentType::Matrix3x3F32
+            | ComponentType::SpatialPosF32
+            | ComponentType::SpatialMotionF32
+            | ComponentType::QuaternionF32 => ElementType::F32,
+            ComponentType::Vector3F64
+            | ComponentType::Matrix3x3F64
+            | ComponentType::QuaternionF64
+            | ComponentType::SpatialPosF64
+            | ComponentType::SpatialMotionF64 => ElementType::F64,
+            ComponentType::Filter => ElementType::U8,
+        }
+    }
+
+    pub fn dims(self) -> heapless::Vec<i64, 2> {
+        match self {
+            ComponentType::U8
+            | ComponentType::U16
+            | ComponentType::U32
+            | ComponentType::U64
+            | ComponentType::I8
+            | ComponentType::I16
+            | ComponentType::I32
+            | ComponentType::I64
+            | ComponentType::Bool
+            | ComponentType::F32
+            | ComponentType::F64 => heapless::Vec::from_slice(&[]).unwrap(),
+            ComponentType::String | ComponentType::Bytes => {
+                heapless::Vec::from_slice(&[-1]).unwrap()
+            }
+            ComponentType::Vector3F32 | ComponentType::Vector3F64 => {
+                heapless::Vec::from_slice(&[3]).unwrap()
+            }
+            ComponentType::Matrix3x3F32 | ComponentType::Matrix3x3F64 => {
+                heapless::Vec::from_slice(&[3, 3]).unwrap()
+            }
+            ComponentType::QuaternionF32 | ComponentType::QuaternionF64 => {
+                heapless::Vec::from_slice(&[4]).unwrap()
+            }
+            ComponentType::SpatialPosF32 | ComponentType::SpatialPosF64 => {
+                heapless::Vec::from_slice(&[7]).unwrap()
+            }
+            ComponentType::SpatialMotionF32 | ComponentType::SpatialMotionF64 => {
+                heapless::Vec::from_slice(&[6]).unwrap()
+            }
+            ComponentType::Filter => heapless::Vec::from_slice(&[9]).unwrap(),
+        }
     }
 }
---
+++ libs/nox-ecs-macros/src/archetype.rs
@@ -0,0 +1,56 @@
+use darling::ast::{self};
+use darling::{FromDeriveInput, FromField};
+use proc_macro::TokenStream;
+use quote::quote;
+use syn::{parse_macro_input, DeriveInput, Generics, Ident};
+
+#[derive(Debug, FromDeriveInput)]
+#[darling(attributes(nox), supports(struct_named))]
+pub struct Archetype {
+    ident: Ident,
+    generics: Generics,
+    data: ast::Data<(), Field>,
+}
+
+#[derive(Debug, FromField)]
+struct Field {
+    ident: Option<syn::Ident>,
+    ty: syn::Type,
+}
+
+pub fn archetype(input: TokenStream) -> TokenStream {
+    let crate_name = crate::nox_ecs_crate_name();
+    let input = parse_macro_input!(input as DeriveInput);
+    let Archetype {
+        ident,
+        generics,
+        data,
+    } = Archetype::from_derive_input(&input).unwrap();
+    let fields = data.take_struct().unwrap();
+    let tys = fields.iter().map(|f| f.ty.clone()).collect::<Vec<_>>();
+    let idents = fields
+        .iter()
+        .map(|f| f.ident.clone().unwrap())
+        .collect::<Vec<_>>();
+    let where_clause = &generics.where_clause;
+    quote! {
+        impl #crate_name::Archetype for #ident #generics #where_clause {
+            fn component_ids() -> Vec<#crate_name::elodin_conduit::ComponentId> {
+                use #crate_name::Component;
+                vec![#( #tys::component_id(), )*]
+            }
+
+            fn component_tys() -> Vec<#crate_name::elodin_conduit::ComponentType> {
+                use #crate_name::Component;
+                vec![#( #tys::component_type(), )*]
+            }
+
+            fn insert_into_table(self, table: &mut #crate_name::Table<#crate_name::HostStore>) {
+                #(
+                   self.#idents.insert_into_table(table);
+                )*
+            }
+        }
+    }
+    .into()
+}
