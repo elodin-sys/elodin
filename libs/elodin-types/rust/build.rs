c730a6a3df9049c6ccfa09ef7cf758df78bdb241
Akhil Velagapudi <4@4khil.com>
2024-01-15 16:27:58 -0800

    Add support for protoc <= 3.14 (#95)
    The optional keyword is only supported by default in protoc versions >=
    3.15. The "experimental_allow_proto3_optional" flag is needed for older
    versions.
    
    Signed-off-by: Akhil Velagapudi <4@4khil.com>

--- libs/elodin-types/rust/build.rs
+++ libs/elodin-types/rust/build.rs
@@ -12,6 +12,8 @@ fn main() {
     tonic_build::configure()
         .build_client(true)
         .build_server(true)
+        // only needed for libprotoc versions <= 3.14, optional keyword is supported by default in >= 3.15
+        .protoc_arg("--experimental_allow_proto3_optional")
         .file_descriptor_set_path(out_dir.join("elodin_types.bin"))
         .compile(&protos, &[protobuf_dir()])
         .unwrap_or_else(|e| panic!("Failed to compile protos {:?}", e));
