faff6197f0f02b6058098200d9f5d5750803f3e4
Sascha Wise <me@saschawise.com>
2024-02-01 10:44:56 -0800

    Improve nox-py ergonomics and add `Query` (#128)
    * feat(nox-ecs): add ndarray getters and setters
    
    * fix(nox-py): fix map impl
    
    * feat(nox-py): add initial `Query` impl
    
    * feat(nox-py): allow `Query` in system arg
    
    * feat(nox-py): allow returning spare ComponentArray
    
    * chore(nox-py): fix spelling + grammar in comment

--- libs/nox-py/test2.py
+++ libs/nox-py/test2.py
@@ -1,23 +1,32 @@
 import nox_py
 import jax
 import jax.numpy as np
-from nox_py import Component, ComponentType, system, ComponentArray, Archetype, WorldBuilder, Client, ComponentId
+from nox_py import Component, ComponentType, system, ComponentArray, Archetype, WorldBuilder, Client, ComponentId, Query
 from dataclasses import dataclass
 
 X = Component[jax.Array, "x", ComponentType.F32]
 Y = Component[jax.Array, "y", ComponentType.F32]
+E = Component[jax.Array, "e", ComponentType.F32]
 print(X.__metadata__)
 
 @system
 def foo(x: ComponentArray[X]) -> ComponentArray[X]:
-  return x
+  return x.map(lambda x: x * 2)
+
+# @system
+# def bar(y: ComponentArray[Y], x: ComponentArray[X]) -> ComponentArray[X]:
+#   return y.join(x).map(X, lambda x, y: x * y)
 
 @system
-def bar(y: ComponentArray[Y], x: ComponentArray[X]) -> ComponentArray[X]:
-  x.buf += y.buf
-  return x
+def bar(q: Query[X, Y]) -> ComponentArray[X]:
+  return q.map(X, lambda x, y: x * y)
 
-sys = foo.pipe(bar)
+@system
+def baz(q: Query[X, E]) -> ComponentArray[X]:
+  return q.map(X, lambda x, e: x + e)
+
+
+sys = foo.pipe(bar).pipe(baz)
 
 
 @dataclass
@@ -25,12 +34,24 @@ class Test(Archetype):
   x: X
   y: Y
 
+@dataclass
+class Effect(Archetype):
+  e: E
+
 
 client = Client.cpu()
 w = WorldBuilder()
 w.spawn(Test(np.array([1.0], dtype='float32'), np.array([500.0], dtype='float32')))
-w.spawn(Test(np.array([5.0], dtype='float32'), np.array([500.0], dtype='float32')))
+id = w.spawn(Test(np.array([15.0], dtype='float32'), np.array([500.0], dtype='float32')))
+w.spawn_with_entity_id(Effect(np.array([15.0], dtype='float32')), id)
 exec = w.build(sys, client)
 exec.run(client)
-print(exec.column(ComponentId("y")))
-print(exec.column(ComponentId("x")))
+y1 = exec.column_array(ComponentId("y"))
+x1 = exec.column_array(ComponentId("x"))
+print(y1)
+print(x1)
+exec.run(client)
+print(exec.column_array(ComponentId("y")))
+print(exec.column_array(ComponentId("x")))
+print(y1)
+print(x1)
