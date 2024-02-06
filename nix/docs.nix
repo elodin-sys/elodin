a60d729f7696b86683b89c8d99ef12e34e98d025
Akhil Velagapudi <4@4khil.com>
2024-02-06 10:38:29 -0800

    Bump node to 21 to avoid building it from source
    Signed-off-by: Akhil Velagapudi <4@4khil.com>

--- nix/docs.nix
+++ nix/docs.nix
@@ -2,21 +2,9 @@
 let
   nl2nix = import flakeInputs.npmlock2nix { inherit pkgs; };
 
-  build_nodejs = pkgs.callPackage "${flakeInputs.nixpkgs}/pkgs/development/web/nodejs/nodejs.nix" {
-    python = pkgs.python3;
-  };
-
-  # NOTE: Mintlify dependency has a node version requirements ^18.17.0 || ^20.3.0 || >=21.0.0
-  # NOTE: At the moment nodejs_20 is `20.2.0`, nodejs-18_x is `18.16.0`
-  nodejs-20_5 = build_nodejs {
-    enableNpm = true;
-    version = "20.5.1";
-    sha256 = "sha256-Q5xxqi84woYWV7+lOOmRkaVxJYBmy/1FSFhgScgTQZA=";
-  };
-
   node_modules = nl2nix.v2.node_modules {
     src = ../docs/public;
-    nodejs = nodejs-20_5;
+    nodejs = pkgs.nodejs_21;
   };
 
   app = pkgs.stdenv.mkDerivation {
@@ -35,7 +23,7 @@ let
   docker_attrs = {
     name = "elo-docs";
     tag = "latest";
-    contents = [ nodejs-20_5 app pkgs.cacert pkgs.busybox ];
+    contents = [ pkgs.nodejs_21 app pkgs.cacert pkgs.busybox ];
     config = {
       Env = ["SSL_CERT_FILE=/etc/ssl/certs/ca-bundle.crt"];
       Cmd = [ "node_modules/.bin/mintlify" "dev" ];
