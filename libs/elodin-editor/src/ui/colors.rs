7374eb96c4c6e1bb8a7fe40cb172ab515093a963
Andrei Khvalko <andrey.greams@gmail.com>
2024-02-08 17:54:01 +0100

    Use tint with white image for flexibility (#147)

--- libs/elodin-editor/src/ui/colors.rs
+++ libs/elodin-editor/src/ui/colors.rs
@@ -1,7 +1,6 @@
 use bevy_egui::egui::Color32;
 
 pub const SURFACE_SECONDARY: Color32 = Color32::from_rgb(0x1F, 0x1F, 0x1F);
-
 pub const WHITE: Color32 = Color32::WHITE;
-
 pub const GREY_OPACITY_500: Color32 = Color32::from_rgb(0x99, 0x99, 0x99);
+pub const HYPER_RED: Color32 = Color32::from_rgb(0xEE, 0x3A, 0x43);
