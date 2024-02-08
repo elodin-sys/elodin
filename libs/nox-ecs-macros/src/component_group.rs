b17f07b4273357793bee532794c02f23dd2aee49
Sascha Wise <me@saschawise.com>
2024-02-08 10:06:42 -0800

    ELO-73: Add 6 DOF integrator, component groups, derive macros, and many fixes (#143)
    * feat(nox): add macro for FromBuilder & IntoOp
    
    * feat(nox-ecs): add `ComponentGroup` trait
    
    * feat(nox-ecs): add `ComponentGroup` macro
    
    * feat(nox-ecs): recursively impl ComponentGroup
    
    * fix(nox-ecs): make `ComponentGroup` usable
    
    * feat(nox): impl traits for spatial types
    
    * feat(nox-ecs): add join for 2 queries
    
    * feat(nox-ecs): use component group in integrator
    
    * feat(nox-ecs): add six dof test
    
    * fix(nox-ecs): make `IntoOp` work on multi fields
    
    * fix(nox): fix concat shape calc
    
    * fix(nox): fix broadcast dimensions calc
    
    * feat(nox): add debug helpers to Noxpr
    
    * feat(nox): correctly batch tuple
    
    * fix(nox): fix concat batching
    
    * feat(nox-ecs): fix six dof test
    
    * feat(nox-ecs): add ndarray helper to `ColumnRef`
    
    * chore(nox): cleanup clippy lints and warnings

---
+++ libs/nox-ecs-macros/src/component_group.rs
@@ -0,0 +1,64 @@
+use darling::ast::{self};
+use darling::FromDeriveInput;
+use proc_macro::TokenStream;
+use quote::quote;
+use syn::{parse_macro_input, DeriveInput, Generics, Ident};
+
+#[derive(Debug, FromDeriveInput)]
+#[darling(attributes(nox), supports(struct_tuple, struct_named))]
+pub struct ComponentGroup {
+    ident: Ident,
+    generics: Generics,
+    data: ast::Data<(), syn::Field>,
+}
+
+pub fn component_group(input: TokenStream) -> TokenStream {
+    let crate_name = crate::nox_ecs_crate_name();
+    let input = parse_macro_input!(input as DeriveInput);
+    let ComponentGroup {
+        ident,
+        generics,
+        data,
+    } = ComponentGroup::from_derive_input(&input).unwrap();
+    let fields = data.take_struct().unwrap();
+    let where_clause = &generics.where_clause;
+    let params = fields
+        .fields
+        .iter()
+        .map(|f| f.ty.clone())
+        .collect::<Vec<_>>();
+    quote! {
+        impl #generics #crate_name::ComponentGroup for #ident #generics #where_clause {
+            type Params = (Self,);
+            type Append<B> = (Self, B);
+            fn init_params(builder: &mut crate::PipelineBuilder) -> Result<(), #crate_name::Error> {
+                <(#(#params,)*)>::init_params(builder)
+            }
+
+            fn component_arrays<'a>(
+                builder: &'a crate::PipelineBuilder,
+            ) -> impl Iterator<Item = #crate_name::ComponentArray<()>> + 'a {
+                <(#(#params,)*)>::component_arrays(builder)
+            }
+
+            fn component_types() -> impl Iterator<Item = #crate_name::ComponentType> {
+                <(#(#params,)*)>::component_types()
+            }
+
+
+            fn component_ids() -> impl Iterator<Item = #crate_name::ComponentId> {
+                <(#(#params,)*)>::component_ids()
+            }
+
+            fn component_count() -> usize {
+                <(#(#params,)*)>::component_count()
+            }
+
+
+            fn map_axes() -> &'static [usize] {
+                <(#(#params,)*)>::map_axes()
+            }
+        }
+    }
+    .into()
+}
--- libs/nox-ecs-macros/src/lib.rs
+++ libs/nox-ecs-macros/src/lib.rs
@@ -1,9 +1,16 @@
 use proc_macro::TokenStream;
+use proc_macro2::Span;
+use proc_macro_crate::{crate_name, FoundCrate};
+use quote::quote;
+use syn::Ident;
 
 extern crate proc_macro;
 
 mod archetype;
 mod component;
+mod component_group;
+mod from_builder;
+mod into_op;
 
 #[proc_macro_derive(Component, attributes(nox))]
 pub fn component(input: TokenStream) -> TokenStream {
@@ -15,13 +22,35 @@ pub fn archetype(input: TokenStream) -> TokenStream {
     archetype::archetype(input)
 }
 
-pub(crate) fn nox_ecs_crate_name() -> proc_macro2::TokenStream {
-    use proc_macro2::Span;
-    use proc_macro_crate::{crate_name, FoundCrate};
-    use quote::quote;
-    use syn::Ident;
+#[proc_macro_derive(FromBuilder, attributes(nox))]
+pub fn from_builder(input: TokenStream) -> TokenStream {
+    from_builder::from_builder(input)
+}
 
-    let name = crate_name("nox-ecs").expect("my-crate is present in `Cargo.toml`");
+#[proc_macro_derive(IntoOp, attributes(nox))]
+pub fn into_op(input: TokenStream) -> TokenStream {
+    into_op::into_op(input)
+}
+
+#[proc_macro_derive(ComponentGroup, attributes(nox))]
+pub fn component_group(input: TokenStream) -> TokenStream {
+    component_group::component_group(input)
+}
+
+pub(crate) fn nox_ecs_crate_name() -> proc_macro2::TokenStream {
+    let name = crate_name("nox-ecs").expect("nox-ecs is present in `Cargo.toml`");
+
+    match name {
+        FoundCrate::Itself => quote!(crate),
+        FoundCrate::Name(name) => {
+            let ident = Ident::new(&name, Span::call_site());
+            quote!( #ident )
+        }
+    }
+}
+
+pub(crate) fn nox_crate_name() -> proc_macro2::TokenStream {
+    let name = crate_name("nox").expect("nox is present in `Cargo.toml`");
 
     match name {
         FoundCrate::Itself => quote!(crate),
