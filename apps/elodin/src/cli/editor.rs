65246f01b942fe7579bd129e731dfe55b7eee2d8
Akhil Velagapudi <4@4khil.com>
2024-02-17 19:54:37 -0800

    Avoid creating new tokio runtime in cli
    `block_in_place` allows bevy to run in the main thread without blocking
    the tokio executor.
    
    Signed-off-by: Akhil Velagapudi <4@4khil.com>

--- apps/elodin/src/cli/editor.rs
+++ apps/elodin/src/cli/editor.rs
@@ -16,35 +16,33 @@ impl Cli {
         use elodin_conduit::bevy_sync::SyncPlugin;
         use elodin_editor::EditorPlugin;
         let (sub, bevy_tx) = ConduitSubscribePlugin::pair();
-        std::thread::spawn(move || {
-            let rt = tokio::runtime::Runtime::new().unwrap();
-            rt.block_on(async move {
-                loop {
-                    let Ok(socket) = TcpStream::connect(args.addr).await else {
-                        tokio::time::sleep(std::time::Duration::from_millis(100)).await;
-                        continue;
-                    };
-                    let (rx_socket, tx_socket) = socket.into_split();
-                    if let Err(err) = elodin_conduit::bevy::handle_socket(
-                        bevy_tx.clone(),
-                        tx_socket,
-                        rx_socket,
-                        DEFAULT_SUB_FILTERS,
-                    )
-                    .await
-                    {
-                        tracing::warn!(?err, "socket error");
-                    }
+
+        tokio::spawn(async move {
+            loop {
+                let Ok(socket) = TcpStream::connect(args.addr).await else {
+                    tokio::time::sleep(std::time::Duration::from_millis(100)).await;
+                    continue;
+                };
+                let (rx_socket, tx_socket) = socket.into_split();
+                if let Err(err) = elodin_conduit::bevy::handle_socket(
+                    bevy_tx.clone(),
+                    tx_socket,
+                    rx_socket,
+                    DEFAULT_SUB_FILTERS,
+                )
+                .await
+                {
+                    tracing::warn!(?err, "socket error");
                 }
-            });
+            }
         });
+
         let mut app = App::new();
-        app.add_plugins(EditorPlugin)
-            .add_plugins(SyncPlugin {
-                plugin: sub,
-                subscriptions: Subscriptions::default(),
-            })
-            .run();
+        app.add_plugins(EditorPlugin).add_plugins(SyncPlugin {
+            plugin: sub,
+            subscriptions: Subscriptions::default(),
+        });
+        tokio::task::block_in_place(|| app.run());
 
         Ok(())
     }
