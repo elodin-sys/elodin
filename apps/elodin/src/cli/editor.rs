3fdb2db8a16989a4f2c2a573b251a856e3c48467
Akhil Velagapudi <4@4khil.com>
2024-02-20 12:14:03 -0800

    Use async-in-sync instead of sync-in-async
    Signed-off-by: Akhil Velagapudi <4@4khil.com>

--- apps/elodin/src/cli/editor.rs
+++ apps/elodin/src/cli/editor.rs
@@ -2,8 +2,8 @@ use std::net::SocketAddr;
 use tokio::net::TcpStream;
 
 use crate::Cli;
-use bevy::{prelude::App, utils::tracing};
-use elodin_conduit::server::handle_socket;
+use bevy::{prelude::*, utils::tracing};
+use elodin_conduit::{client::MsgPair, server::handle_socket};
 
 #[derive(clap::Args, Clone)]
 pub struct Args {
@@ -11,31 +11,54 @@ pub struct Args {
 }
 
 impl Cli {
-    pub async fn editor(&self, args: Args) -> anyhow::Result<()> {
+    pub fn editor(&self, args: Args) -> anyhow::Result<()> {
         use elodin_conduit::bevy::{ConduitSubscribePlugin, Subscriptions};
         use elodin_conduit::bevy_sync::SyncPlugin;
         use elodin_editor::EditorPlugin;
         let (sub, bevy_tx) = ConduitSubscribePlugin::pair();
-        tokio::spawn(async move {
-            loop {
-                let Ok(socket) = TcpStream::connect(args.addr).await else {
-                    tokio::time::sleep(std::time::Duration::from_millis(100)).await;
-                    continue;
-                };
-                let (rx_socket, tx_socket) = socket.into_split();
-                if let Err(err) = handle_socket(bevy_tx.clone(), tx_socket, rx_socket).await {
-                    tracing::warn!(?err, "socket error");
-                }
-            }
-        });
 
-        let mut app = App::new();
-        app.add_plugins(EditorPlugin).add_plugins(SyncPlugin {
-            plugin: sub,
-            subscriptions: Subscriptions::default(),
-        });
-        tokio::task::block_in_place(|| app.run());
+        App::new()
+            .add_plugins(SimClient {
+                addr: args.addr,
+                bevy_tx,
+            })
+            .add_plugins(EditorPlugin)
+            .add_plugins(SyncPlugin {
+                plugin: sub,
+                subscriptions: Subscriptions::default(),
+            })
+            .run();
 
         Ok(())
+    }
+}
+
+#[derive(Clone)]
+struct SimClient {
+    addr: SocketAddr,
+    bevy_tx: flume::Sender<MsgPair>,
+}
+
+impl Plugin for SimClient {
+    fn build(&self, _: &mut App) {
+        let c = self.clone();
+        std::thread::spawn(move || {
+            let rt = tokio::runtime::Builder::new_current_thread()
+                .enable_all()
+                .build()
+                .expect("tokio runtime failed to start");
+            rt.block_on(async move {
+                loop {
+                    let Ok(socket) = TcpStream::connect(c.addr).await else {
+                        tokio::time::sleep(std::time::Duration::from_millis(100)).await;
+                        continue;
+                    };
+                    let (rx_socket, tx_socket) = socket.into_split();
+                    if let Err(err) = handle_socket(c.bevy_tx.clone(), tx_socket, rx_socket).await {
+                        tracing::warn!(?err, "socket error");
+                    }
+                }
+            });
+        });
     }
 }
