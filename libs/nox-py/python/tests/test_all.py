e849f8146caf3dcff0babaf93a186438df1a76ee
Akhil Velagapudi <4@4khil.com>
2024-02-26 11:27:22 -0800

    Replace `elodin-py` with `nox-py` in `sim-agent` (#182)
    * feat(nox-py): use "build" as serialize command
    
    The build functionality is useful outside of monte-carlo runs.
    
    Signed-off-by: Akhil Velagapudi <4@4khil.com>
    
    * feat(nox-py): add better gravity to 6DOF example
    
    Also, use `from elodin import *`.
    
    Signed-off-by: Akhil Velagapudi <4@4khil.com>
    
    * feat(sim-agent): add elodin (nox-py) Python module
    
    Make embedding the CLI optional as it's not needed in sim-agent.
    
    Signed-off-by: Akhil Velagapudi <4@4khil.com>
    
    * chore(atc): remove double-pend template
    
    Also, replace three-body template with the 6DOF example.
    
    Signed-off-by: Akhil Velagapudi <4@4khil.com>
    
    * feat(sim-agent): replace elodin-py with nox-py
    
    Signed-off-by: Akhil Velagapudi <4@4khil.com>
    
    * chore: delete elodin-core and elodin-py
    
    Signed-off-by: Akhil Velagapudi <4@4khil.com>
    
    * feat(sim-agent): ser/de exec instead of using PyO3
    
    Add /tmp to sandboxes so that tempfile works.
    
    Signed-off-by: Akhil Velagapudi <4@4khil.com>
    
    * feat(nox-ecs): ser/de asset store
    
    Signed-off-by: Akhil Velagapudi <4@4khil.com>
    
    * chore(conduit): add more logs
    
    Signed-off-by: Akhil Velagapudi <4@4khil.com>
    
    * chore(nox-ecs): make conduit send/recv infallible
    
    Signed-off-by: Akhil Velagapudi <4@4khil.com>
    
    * chore(nox-ecs): reduce log level of send failures
    
    This can happen easily, and is expected behavior.
    
    Signed-off-by: Akhil Velagapudi <4@4khil.com>
    
    * chore: remove unused dependencies
    
    Signed-off-by: Akhil Velagapudi <4@4khil.com>
    
    * chore(conduit): remove unused ConduitTcpSubscriber
    
    Signed-off-by: Akhil Velagapudi <4@4khil.com>
    
    * fix(conduit): initiate default subs from editor
    
    Signed-off-by: Akhil Velagapudi <4@4khil.com>
    
    * fix(conduit): send ctrl msgs in response to ticks
    
    Signed-off-by: Akhil Velagapudi <4@4khil.com>
    
    * fix(editor): wait for ws ready before sending
    
    Set callbacks as per
    https://rustwasm.github.io/wasm-bindgen/examples/websockets.html.
    
    Also, propagate more errors to the console for easier debugging.
    
    Signed-off-by: Akhil Velagapudi <4@4khil.com>
    
    * chore(atc): add more logs in the ws<->sim path
    
    Also, filter out some of the noise db, redmq logs.
    
    Signed-off-by: Akhil Velagapudi <4@4khil.com>
    
    * fix(conduit): clean up stale conns + subs
    
    Signed-off-by: Akhil Velagapudi <4@4khil.com>
    
    * fix(nox-ecs): dedup connections
    
    Signed-off-by: Akhil Velagapudi <4@4khil.com>
    
    * feat(conduit): move sub init to event loop
    
    Initialization subscriptions at the transport layer is weird. It should
    be done at the application layer (e.g. bevy client, sim agent server).
    Add two new control message types (Connect, and StartSim) to facilitate
    this.
    
    Agent receives Connect from transport and responds with StartSim. Bevy
    client receives the StartSim and responds with subscriptions. This lays
    a foundation for fixing "Update Sim" as it requires re-negotiation at
    the application/protocol level rather than the transport level.
    
    Signed-off-by: Akhil Velagapudi <4@4khil.com>
    
    ---------
    
    Signed-off-by: Akhil Velagapudi <4@4khil.com>

--- libs/nox-py/python/tests/test_all.py
+++ libs/nox-py/python/tests/test_all.py
@@ -1,8 +1,7 @@
 import pytest
-import elodin
 import jax
 import jax.numpy as np
-from elodin import Component, ComponentType, system, ComponentArray, Archetype, WorldBuilder, Client, ComponentId, Query, WorldPos, WorldAccel, WorldVel, Inertia, Force, Body, six_dof, Material, Mesh
+from elodin import *
 from dataclasses import dataclass
 
 def test_basic_system():
@@ -52,8 +51,6 @@ def test_six_dof() :
     w.spawn(Body(
         world_pos = WorldPos.from_linear(np.array([0.,0.,0.])),
         world_vel = WorldVel.from_linear(np.array([1.,0.,0.])),
-        world_accel = WorldVel.from_linear(np.array([0.,0.,0.])),
-        force = Force.zero(),
         inertia = Inertia.from_mass(1.0),
         mesh = w.insert_asset(Mesh.cuboid(1.0, 1.0, 1.0)),
         material = w.insert_asset(Material.color(1.0, 1.0, 1.0))
