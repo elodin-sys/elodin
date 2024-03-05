3284a6844c52c5ce69fcb2835fda655d97150563
Akhil Velagapudi <4@4khil.com>
2024-03-05 15:01:41 -0800

    chore: remove unused deps (#217)
    Signed-off-by: Akhil Velagapudi <4@4khil.com>

--- nix/shell.nix
+++ nix/shell.nix
@@ -27,8 +27,6 @@
           fontconfig
           maturin
         ];
-      LIBCLANG_PATH = "${pkgs.llvmPackages_14.libclang.lib}/lib";
-      BINDGEN_EXTRA_CLANG_ARGS = with pkgs; ''${lib.optionalString stdenv.cc.isGNU "-isystem ${stdenv.cc.cc}/include/c++/${lib.getVersion stdenv.cc.cc} -isystem ${stdenv.cc.cc}/include/c++/${lib.getVersion stdenv.cc.cc}/${stdenv.hostPlatform.config} -idirafter ${stdenv.cc.cc}/lib/gcc/${stdenv.hostPlatform.config}/${lib.getVersion stdenv.cc.cc}/include"}'';
       doCheck = false;
     };
     elixir = pkgs.mkShell {
