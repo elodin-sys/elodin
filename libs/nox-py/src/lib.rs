dbaa01b80efead5000582624cfa0d62cb60b0ec4
Akhil Velagapudi <4@4khil.com>
2024-03-13 09:56:11 -0700

    feat(nox-py): impl __str__ for EntityId
    Signed-off-by: Akhil Velagapudi <4@4khil.com>

--- libs/nox-py/src/lib.rs
+++ libs/nox-py/src/lib.rs
@@ -1,3 +1,4 @@
+use core::fmt;
 use std::{
     collections::{hash_map::Entry, BTreeMap},
     marker::PhantomData,
@@ -783,6 +784,16 @@ impl EntityId {
     #[new]
     fn new(id: u64) -> Self {
         EntityId { inner: id.into() }
+    }
+
+    fn __str__(&self) -> String {
+        self.to_string()
+    }
+}
+
+impl fmt::Display for EntityId {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        self.inner.0.fmt(f)
     }
 }
 
