7b4866c294c93544e39934866e6b029f8247f2ca
Akhil Velagapudi <4@4khil.com>
2024-04-01 12:00:38 -0700

    feat(sim-builder): use pyo3 run for faster builds (#307)
    Signed-off-by: Akhil Velagapudi <4@4khil.com>

--- services/sim-builder/src/main.rs
+++ services/sim-builder/src/main.rs
@@ -1,4 +1,5 @@
 use tokio::task::JoinSet;
+use tokio_util::sync::CancellationToken;
 use tonic::transport::{server::Routes, Server};
 
 mod api;
@@ -9,6 +10,7 @@ const MAX_MESSAGE_SIZE: usize = 256 * 1024; // 256 KiB
 async fn main() -> anyhow::Result<()> {
     tracing_subscriber::fmt::init();
     let mut tasks = JoinSet::new();
+    let cancel_token = CancellationToken::new();
 
     let (mut health_reporter, health_service) = tonic_health::server::health_reporter();
     api::Service::set_serving(&mut health_reporter).await;
@@ -34,8 +36,12 @@ async fn main() -> anyhow::Result<()> {
             tracing::info!(?tcp_addr, "listening");
             let tcp_stream = tokio_stream::wrappers::TcpListenerStream::new(tcp_listener);
             let router = server.add_routes(routes.clone());
+            let cancelled = cancel_token.child_token().cancelled_owned();
             tasks.spawn(async move {
-                if let Err(err) = router.serve_with_incoming(tcp_stream).await {
+                if let Err(err) = router
+                    .serve_with_incoming_shutdown(tcp_stream, cancelled)
+                    .await
+                {
                     tracing::error!(?err, "tcp stream terminated");
                 }
             });
@@ -51,8 +57,12 @@ async fn main() -> anyhow::Result<()> {
                 tracing::info!(?vsock_addr, "listening");
                 let vsock_stream = vsock_listener.incoming();
                 let router = server.add_routes(routes);
+                let cancelled = cancel_token.child_token().cancelled_owned();
                 tasks.spawn(async move {
-                    if let Err(err) = router.serve_with_incoming(vsock_stream).await {
+                    if let Err(err) = router
+                        .serve_with_incoming_shutdown(vsock_stream, cancelled)
+                        .await
+                    {
                         tracing::error!(?err, "vsock stream terminated");
                     }
                 });
@@ -60,6 +70,16 @@ async fn main() -> anyhow::Result<()> {
             Err(err) => tracing::warn!(?err, "failed to bind vsock"),
         }
     }
+
+    // shutdown on ctrl-c
+    // this is needed because pyo3 hijacks signal handling
+    tokio::spawn(async move {
+        tokio::signal::ctrl_c()
+            .await
+            .expect("failed to listen for ctrl-c");
+        tracing::info!("received ctrl-c, initiating shutdown");
+        cancel_token.cancel();
+    });
 
     while let Some(res) = tasks.join_next().await {
         res.unwrap();
