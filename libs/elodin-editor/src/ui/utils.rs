2c21e872f8f5b929517b3fd4a4f528c5f3d68744
Akhil Velagapudi <4@4khil.com>
2024-04-24 14:39:27 -0700

    feat(editor): drop milliseconds from x-axis (#394)
    * fix(editor): avoid overflow on bad data
    
    Signed-off-by: Akhil Velagapudi <4@4khil.com>
    
    * feat(editor): drop milliseconds from x-axis
    
    This prevents squashing and is consistent with the design.
    
    Fixes: ELO-260
    
    Signed-off-by: Akhil Velagapudi <4@4khil.com>
    
    ---------
    
    Signed-off-by: Akhil Velagapudi <4@4khil.com>

--- libs/elodin-editor/src/ui/utils.rs
+++ libs/elodin-editor/src/ui/utils.rs
@@ -109,7 +109,7 @@ pub fn format_num(mut num: f64) -> String {
     let width: usize = 8;
     // need 2 characters for the sign and the decimal point
     let digit_width = width - 2;
-    let digits = num.abs().log10().ceil() as usize + 1;
+    let digits = (num.abs().log10().ceil() as usize).saturating_add(1);
     let precision = digit_width.saturating_sub(digits).min(3).max(1);
     // round to the nearest multiple of 10^(-precision)
     num = (num * 10.0_f64.powi(precision as i32)).round() / 10.0_f64.powi(precision as i32);
