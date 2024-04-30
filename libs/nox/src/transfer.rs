f98847406f37e5e9ae773eb68b1b900964d08ad5
Nestor Demeure <nestordemeure@gmail.com>
2024-04-30 16:19:42 -0700

    commented transfer.rs

--- libs/nox/src/transfer.rs
+++ libs/nox/src/transfer.rs
@@ -1,18 +1,26 @@
+//! Traits and implementations for data transfer between host and XLA devices, buffer management, and XLA client operations.
 use std::{borrow::Borrow, ops::Deref};
 
 use crate::{Buffer, Client, Dim, Noxpr, Op, Tensor, TensorItem};
 
+/// Defines a trait for converting host data types into computation graph nodes.
 pub trait FromHost {
+    /// Type of the host data that can be converted.
     type HostTy;
 
+    /// Converts a native host type into an instance of the implementing type, using a provided client.
     fn from_host(client: &Client, native: Self::HostTy) -> Self;
 }
 
+/// Defines a trait for constructing objects from PJRT buffers.
 pub trait FromPjrtBuffer {
+    /// Constructs an object from PJRT buffers.
     fn from_pjrt(pjrt: Vec<xla::PjRtBuffer>) -> Self;
 }
 
+/// Provides a mechanism to retrieve a reference to an underlying PJRT buffer.
 pub trait AsBuffer {
+    /// Returns a reference to an associated PJRT buffer.
     fn as_buffer(&self) -> &xla::PjRtBuffer;
 }
 
@@ -22,11 +30,15 @@ impl<'a, A: AsBuffer> AsBuffer for &'a mut A {
     }
 }
 
+/// A trait for converting computation graph nodes into instances of the implementing type.
 pub trait FromOp {
+    /// Constructs an instance of the implementing type from a computation graph node.
     fn from_op(noxpr: Noxpr) -> Self;
 }
 
+/// A trait for converting instances into computation graph nodes.
 pub trait IntoOp {
+    /// Converts an instance into a computation graph node.
     fn into_op(self) -> Noxpr;
 }
 
@@ -36,17 +48,29 @@ impl IntoOp for () {
     }
 }
 
+/// Defines a trait to represent the buffer form of a type.
 pub trait BufferForm {
+    /// The type of the buffer that represents the data.
     type BufferTy;
 }
 
+/// Defines operations for managing buffer arguments, allowing for checking mutable borrow status and buffer manipulation.
 pub trait BufferArg<BufferTy> {
+    /// Determines if the buffer is currently mutably borrowed.
+    ///
+    /// By default, returns false, indicating no mutable borrow.
     fn is_mut_borrowed() -> bool {
         false
     }
 
+    /// Retrieves the buffer associated with this object, possibly in a borrowed state.
+    ///
+    /// `client`: The client associated with the buffer's lifecycle.
     fn as_buffer(&self, client: &Client) -> MaybeOwned<'_, xla::PjRtBuffer>;
 
+    /// Replaces the current buffer with a new one.
+    ///
+    /// `new_buffer`: The new buffer to set.
     fn replace_buffer(&mut self, _new_buffer: xla::PjRtBuffer) {}
 }
 
