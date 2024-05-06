34453d9fd4d083a1d79da9741f3bf774946f7858
Akhil Velagapudi <4@4khil.com>
2024-05-06 11:28:58 -0700

    fix(examples): use "tick", "world_pos" col names (#423)
    Signed-off-by: Akhil Velagapudi <4@4khil.com>

--- libs/nox-py/examples/ball-plot.py
+++ libs/nox-py/examples/ball-plot.py
@@ -93,22 +93,18 @@ def run(seed: int) -> pl.DataFrame:
         exec.run(client)
     return exec.history()
 
-
-# This is a bit awkward, support df["world_pos"]
-world_pos_id = str(el.Component.id(el.WorldPos))
-
 fig, ax = plt.subplots()
 
 for i in range(0, 20):
     df = run(i)
-    df = df.sort("time").select(["time", world_pos_id]).drop_nulls()
+    df = df.sort("tick").select(["tick", "world_pos"]).drop_nulls()
     df = df.with_columns(
-        pl.col(world_pos_id).arr.get(4).alias("x"),
-        pl.col(world_pos_id).arr.get(5).alias("y"),
-        pl.col(world_pos_id).arr.get(6).alias("z"),
+        pl.col("world_pos").arr.get(4).alias("x"),
+        pl.col("world_pos").arr.get(5).alias("y"),
+        pl.col("world_pos").arr.get(6).alias("z"),
     )
     distance = np.linalg.norm(df.select(["x", "y", "z"]).to_numpy(), axis=1)
     df = df.with_columns(pl.Series(distance).alias("distance"))
-    ax.plot(df["time"], df["distance"])
+    ax.plot(df["tick"], df["distance"])
 
 plt.show()
--- libs/nox-py/examples/ball.py
+++ libs/nox-py/examples/ball.py
@@ -88,12 +88,11 @@ w.run(sys)
 
 
 def test_origin_drift(df: pl.DataFrame):
-    world_pos_id = str(el.Component.id(el.WorldPos))
-    df = df.sort("time").select(["time", world_pos_id]).drop_nulls()
+    df = df.sort("tick").select(["tick", "world_pos"]).drop_nulls()
     df = df.with_columns(
-        pl.col(world_pos_id).arr.get(4).alias("x"),
-        pl.col(world_pos_id).arr.get(5).alias("y"),
-        pl.col(world_pos_id).arr.get(6).alias("z"),
+        pl.col("world_pos").arr.get(4).alias("x"),
+        pl.col("world_pos").arr.get(5).alias("y"),
+        pl.col("world_pos").arr.get(6).alias("z"),
     )
     distance = np.linalg.norm(df.select(["x", "z"]).to_numpy(), axis=1)
     df = df.with_columns(pl.Series(distance).alias("distance"))
