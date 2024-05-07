c0c66972040d45de5cc728dd8d08a3dd95e9fb9e
Akhil Velagapudi <4@4khil.com>
2024-05-06 17:11:35 -0700

    chore: fix clippy 1.78 lints (#425)
    Signed-off-by: Akhil Velagapudi <4@4khil.com>

--- libs/nox/src/param.rs
+++ libs/nox/src/param.rs
@@ -77,12 +77,12 @@ pub trait Repr {
         <BroadcastedDim<D1, D2> as ArrayDim>::Buf<MaybeUninit<T>>:
             ArrayBufUnit<T, Init = <BroadcastedDim<D1, D2> as ArrayDim>::Buf<T>>;
 
-    fn dot<T: Field, D1, D2>(
+    fn dot<T, D1, D2>(
         left: &Self::Inner<T, D1>,
         right: &Self::Inner<T, D2>,
     ) -> Self::Inner<T, <ShapeConstraint as DotDim<D1, D2>>::Output>
     where
-        T: Div<Output = T> + Copy,
+        T: Field + Div<Output = T> + Copy,
         D1: Dim + ArrayDim,
         D2: Dim + ArrayDim,
         ShapeConstraint: DotDim<D1, D2>,
@@ -90,13 +90,13 @@ pub trait Repr {
         <DottedDim<D1, D2> as ArrayDim>::Buf<MaybeUninit<T>>:
             ArrayBufUnit<T, Init = <DottedDim<D1, D2> as ArrayDim>::Buf<T>>;
 
-    fn concat_many<T1: Field, D1: Dim, const N: usize>(
+    fn concat_many<T1: Field, D1, const N: usize>(
         args: [&Self::Inner<T1, D1>; N],
     ) -> Self::Inner<T1, ConcatManyDim<D1, N>>
     where
         DefaultMappedDim<D1>: nalgebra::DimMul<Const<N>> + nalgebra::Dim,
         D1::DefaultMapDim: MapDim<D1>,
-        D1: DefaultMap,
+        D1: Dim + DefaultMap,
         MulDim<DefaultMappedDim<D1>, Const<N>>: Dim,
         <<D1 as DefaultMap>::DefaultMapDim as MapDim<D1>>::MappedDim: nalgebra::Dim,
         ConcatManyDim<D1, N>: Dim,
@@ -180,12 +180,12 @@ impl Repr for Literal {
         todo!()
     }
 
-    fn dot<T: Field, D1, D2>(
+    fn dot<T, D1, D2>(
         _left: &Self::Inner<T, D1>,
         _right: &Self::Inner<T, D2>,
     ) -> Self::Inner<T, <ShapeConstraint as DotDim<D1, D2>>::Output>
     where
-        T: Div<Output = T> + Copy,
+        T: Field + Div<Output = T> + Copy,
         D1: Dim + ArrayDim,
         D2: Dim + ArrayDim,
         ShapeConstraint: DotDim<D1, D2>,
@@ -196,14 +196,14 @@ impl Repr for Literal {
         todo!()
     }
 
-    fn concat_many<T1: Field, D1: Dim, const N: usize>(
+    fn concat_many<T1: Field, D1, const N: usize>(
         _args: [&Self::Inner<T1, D1>; N],
     ) -> Self::Inner<T1, ConcatManyDim<D1, N>>
     where
         DefaultMappedDim<D1>: nalgebra::DimMul<Const<N>> + nalgebra::Dim,
         D1::DefaultMapDim: MapDim<D1>,
         D1::DefaultMapDim: MapDim<D1>,
-        D1: DefaultMap,
+        D1: Dim + DefaultMap,
         MulDim<DefaultMappedDim<D1>, Const<N>>: Dim,
         <<D1 as DefaultMap>::DefaultMapDim as MapDim<D1>>::MappedDim: nalgebra::Dim,
         ConcatManyDim<D1, N>: Dim,
@@ -293,12 +293,12 @@ impl Repr for Buffer {
         todo!()
     }
 
-    fn dot<T: Field, D1, D2>(
+    fn dot<T, D1, D2>(
         _left: &Self::Inner<T, D1>,
         _right: &Self::Inner<T, D2>,
     ) -> Self::Inner<T, <ShapeConstraint as DotDim<D1, D2>>::Output>
     where
-        T: Div<Output = T> + Copy,
+        T: Field + Div<Output = T> + Copy,
         D1: Dim + ArrayDim,
         D2: Dim + ArrayDim,
         ShapeConstraint: DotDim<D1, D2>,
@@ -309,14 +309,14 @@ impl Repr for Buffer {
         todo!()
     }
 
-    fn concat_many<T1: Field, D1: Dim, const N: usize>(
+    fn concat_many<T1: Field, D1, const N: usize>(
         _args: [&Self::Inner<T1, D1>; N],
     ) -> Self::Inner<T1, ConcatManyDim<D1, N>>
     where
         DefaultMappedDim<D1>: nalgebra::DimMul<Const<N>> + nalgebra::Dim,
         D1::DefaultMapDim: MapDim<D1>,
         D1::DefaultMapDim: MapDim<D1>,
-        D1: DefaultMap,
+        D1: Dim + DefaultMap,
         MulDim<DefaultMappedDim<D1>, Const<N>>: Dim,
         <<D1 as DefaultMap>::DefaultMapDim as MapDim<D1>>::MappedDim: nalgebra::Dim,
         ConcatManyDim<D1, N>: Dim,
@@ -422,14 +422,14 @@ impl Repr for Op {
         Noxpr::dot(left.clone(), right)
     }
 
-    fn concat_many<T1: Field, D1: Dim, const N: usize>(
+    fn concat_many<T1: Field, D1, const N: usize>(
         args: [&Self::Inner<T1, D1>; N],
     ) -> Self::Inner<T1, ConcatManyDim<D1, N>>
     where
         DefaultMappedDim<D1>: nalgebra::DimMul<Const<N>> + nalgebra::Dim,
         D1::DefaultMapDim: MapDim<D1>,
         D1::DefaultMapDim: MapDim<D1>,
-        D1: DefaultMap,
+        D1: Dim + DefaultMap,
         MulDim<DefaultMappedDim<D1>, Const<N>>: Dim,
         <<D1 as DefaultMap>::DefaultMapDim as MapDim<D1>>::MappedDim: nalgebra::Dim,
         ConcatManyDim<D1, N>: Dim,
