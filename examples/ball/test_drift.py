072658bca84a4b353d72fca9eb1f0c59ed566fb2
Akhil Velagapudi <4@4khil.com>
2024-06-07 16:08:49 -0400

    feat(ball): move ball examples to top-level dir (#494)
    Also, refactor into a more standard-ish Python module with code reuse.
    Previously, almost all of the sim code was duplicated across ball.py and
    ball-plot.py unnecessarily.
    
    The following commands should work:
    
    elodin editor examples/ball/main.py
    python examples/ball/plot.py
    
    Signed-off-by: Akhil Velagapudi <4@4khil.com>

---
+++ examples/ball/test_drift.py
@@ -0,0 +1,16 @@
+import polars as pl
+import numpy as np
+from typing import cast
+
+
+def test_origin_drift(df: pl.DataFrame):
+    df = df.sort("tick").select(["tick", "world_pos"]).drop_nulls()
+    df = df.with_columns(
+        pl.col("world_pos").arr.get(4).alias("x"),
+        pl.col("world_pos").arr.get(5).alias("y"),
+        pl.col("world_pos").arr.get(6).alias("z"),
+    )
+    distance = np.linalg.norm(df.select(["x", "z"]).to_numpy(), axis=1)
+    df = df.with_columns(pl.Series(distance).alias("distance"))
+    max_dist = cast(int, df["distance"].max())
+    assert max_dist < 2
