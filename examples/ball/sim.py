d581e26894a0607f4387f1ed75cb8242440ebc91
Akhil Velagapudi <4@4khil.com>
2024-06-07 16:08:49 -0400

    feat(ball): move ball examples to top-level dir (#494)
    Also, refactor into a more standard-ish Python module with code reuse.
    Previously, almost all of the sim code was duplicated across ball.py and
    ball-plot.py unnecessarily.
    
    The following commands should work:
    
    elodin editor examples/ball/main.py
    python examples/ball/plot.py
    
    Signed-off-by: Akhil Velagapudi <4@4khil.com>

--- examples/ball/sim.py
+++ examples/ball/sim.py
@@ -3,9 +3,6 @@ import typing
 import elodin as el
 from jax import numpy as jnp
 from jax import random
-import polars as pl
-import numpy as np
-from typing import cast
 from jax.numpy import linalg as la
 from dataclasses import field
 
@@ -42,7 +39,9 @@ def apply_wind(
     def apply_wind_inner(f, v):
         v_diff = w[0] - v.linear()
         v_diff_dir = v_diff / la.norm(v_diff)
-        return el.Force.from_linear(f.force() + 0.2 * 0.5 * v_diff**2 * v_diff_dir)
+        return el.SpatialForce.from_linear(
+            f.force() + 0.2 * 0.5 * v_diff**2 * v_diff_dir
+        )
 
     return q.map(
         el.Force,
@@ -52,14 +51,16 @@ def apply_wind(
 
 @el.map
 def gravity(f: el.Force, inertia: el.Inertia) -> el.Force:
-    return f + el.Force.from_linear(jnp.array([0.0, inertia.mass() * -9.81, 0.0]))
+    return f + el.SpatialForce.from_linear(
+        jnp.array([0.0, inertia.mass() * -9.81, 0.0])
+    )
 
 
 @el.map
 def bounce(p: el.WorldPos, v: el.WorldVel) -> el.WorldVel:
     return jax.lax.cond(
         jax.lax.max(p.linear()[1], v.linear()[1]) < 0.0,
-        lambda _: el.WorldVel.from_linear(
+        lambda _: el.SpatialMotion.from_linear(
             v.linear() * jnp.array([1.0, -1.0, 1.0]) * 0.85
         ),
         lambda _: v,
@@ -67,39 +68,33 @@ def bounce(p: el.WorldPos, v: el.WorldVel) -> el.WorldVel:
     )
 
 
-w = el.World()
-w.spawn(Globals(), name="Globals")
-w.spawn(
-    [
-        el.Body(world_pos=el.SpatialTransform.from_linear(jnp.array([0.0, 6.0, 0.0]))),
-        w.shape(el.Mesh.sphere(0.4), el.Material.color(12.7, 9.2, 0.5)),
-    ],
-    name="Ball",
-)
-w.spawn(
-    el.Panel.viewport(
-        track_rotation=False,
-        active=True,
-        pos=[6.0, 3.0, 6.0],
-        looking_at=[0.0, 1.0, 0.0],
-        show_grid=True,
-        hdr=True,
-    ),
-    name="Viewport",
-)
-effectors = gravity.pipe(apply_wind)
-sys = sample_wind.pipe(bounce.pipe(el.six_dof(TIME_STEP, effectors)))
-w.run(sys)
-
-
-def test_origin_drift(df: pl.DataFrame):
-    df = df.sort("tick").select(["tick", "world_pos"]).drop_nulls()
-    df = df.with_columns(
-        pl.col("world_pos").arr.get(4).alias("x"),
-        pl.col("world_pos").arr.get(5).alias("y"),
-        pl.col("world_pos").arr.get(6).alias("z"),
+def world(seed: int = 0) -> el.World:
+    world = el.World()
+    world.spawn(Globals(seed=jnp.int64(seed)), name="Globals")
+    world.spawn(
+        [
+            el.Body(
+                world_pos=el.SpatialTransform.from_linear(jnp.array([0.0, 6.0, 0.0]))
+            ),
+            world.shape(el.Mesh.sphere(0.4), el.Material.color(12.7, 9.2, 0.5)),
+        ],
+        name="Ball",
     )
-    distance = np.linalg.norm(df.select(["x", "z"]).to_numpy(), axis=1)
-    df = df.with_columns(pl.Series(distance).alias("distance"))
-    max_dist = cast(int, df["distance"].max())
-    assert max_dist < 2
+    world.spawn(
+        el.Panel.viewport(
+            track_rotation=False,
+            active=True,
+            pos=[6.0, 3.0, 6.0],
+            looking_at=[0.0, 1.0, 0.0],
+            show_grid=True,
+            hdr=True,
+        ),
+        name="Viewport",
+    )
+    return world
+
+
+def system() -> el.System:
+    effectors = gravity.pipe(apply_wind)
+    sys = sample_wind.pipe(bounce.pipe(el.six_dof(TIME_STEP, effectors)))
+    return sys
