56066c80aba48f2be39229cbaa0e63258e7690e2
Akhil Velagapudi <4@4khil.com>
2024-06-11 16:57:54 -0400

    feat(nox-py): expose write-to-dir functionality (#512)
    This is useful for some local analysis use-cases.
    
    Signed-off-by: Akhil Velagapudi <4@4khil.com>

--- examples/ball/plot.py
+++ examples/ball/plot.py
@@ -2,21 +2,31 @@ import polars as pl
 from sim import world, system
 import matplotlib.pyplot as plt
 import numpy as np
+import argparse
 
-exec = world().build(system())
-exec.run(ticks=1200)
+parser = argparse.ArgumentParser()
+parser.add_argument("--seed", type=int, default=0)
+parser.add_argument("--ticks", type=int, default=1200)
+parser.add_argument("--export-dir", type=str, default=None)
+parser.add_argument("--plot", action="store_true")
+args = parser.parse_args()
+
+exec = world(args.seed).build(system())
+exec.run(args.ticks)
 df = exec.history()
 
-fig, ax = plt.subplots()
+if args.export_dir:
+    exec.write_to_dir(args.export_dir)
 
-df = df.sort("tick").select(["tick", "world_pos"]).drop_nulls()
-df = df.with_columns(
-    pl.col("world_pos").arr.get(4).alias("x"),
-    pl.col("world_pos").arr.get(5).alias("y"),
-    pl.col("world_pos").arr.get(6).alias("z"),
-)
-distance = np.linalg.norm(df.select(["x", "y", "z"]).to_numpy(), axis=1)
-df = df.with_columns(pl.Series(distance).alias("distance"))
-ax.plot(df["tick"], df["distance"])
-
-plt.show()
+if args.plot:
+    fig, ax = plt.subplots()
+    df = df.sort("tick").select(["tick", "world_pos"]).drop_nulls()
+    df = df.with_columns(
+        pl.col("world_pos").arr.get(4).alias("x"),
+        pl.col("world_pos").arr.get(5).alias("y"),
+        pl.col("world_pos").arr.get(6).alias("z"),
+    )
+    distance = np.linalg.norm(df.select(["x", "y", "z"]).to_numpy(), axis=1)
+    df = df.with_columns(pl.Series(distance).alias("distance"))
+    ax.plot(df["tick"], df["distance"])
+    plt.show()
