1938c8c9489437bd186aa2a5271816ecfa67896c
Akhil Velagapudi <4@4khil.com>
2024-06-26 15:57:18 -0600

    Add reaction wheel support (#548)
    * feat(cw): add back guidance and control
    
    Signed-off-by: Akhil Velagapudi <4@4khil.com>
    
    * feat(cw): set reaction wheel RPMs
    
    Signed-off-by: Akhil Velagapudi <4@4khil.com>
    
    * feat(cw): set correct sat mass, moment of inertia
    
    Signed-off-by: Akhil Velagapudi <4@4khil.com>
    
    * feat(nox-ecs): log after replay is written
    
    Signed-off-by: Akhil Velagapudi <4@4khil.com>
    
    * feat(cw): tune ekf, update hard iron offsets
    
    Signed-off-by: Akhil Velagapudi <4@4khil.com>
    
    * feat(cw): send rw speeds to sim
    
    Signed-off-by: Akhil Velagapudi <4@4khil.com>
    
    ---------
    
    Signed-off-by: Akhil Velagapudi <4@4khil.com>

--- libs/roci/src/componentize.rs
+++ libs/roci/src/componentize.rs
@@ -40,3 +40,25 @@ where
 
     const MAX_SIZE: usize = T1::MAX_SIZE + T2::MAX_SIZE;
 }
+
+impl<T1, T2, T3> Componentize for (T1, T2, T3)
+where
+    T1: Componentize,
+    T2: Componentize,
+    T3: Componentize,
+{
+    fn sink_columns(&self, output: &mut impl Decomponentize) {
+        self.0.sink_columns(output);
+        self.1.sink_columns(output);
+        self.2.sink_columns(output);
+    }
+
+    fn get_metadata(&self, component_id: ComponentId) -> Option<&Metadata> {
+        self.2
+            .get_metadata(component_id)
+            .or_else(|| self.1.get_metadata(component_id))
+            .or_else(|| self.0.get_metadata(component_id))
+    }
+
+    const MAX_SIZE: usize = T1::MAX_SIZE + T2::MAX_SIZE + T3::MAX_SIZE;
+}
