28c32b88f4fb3f637def968404b9d9f8012d8192
Sascha Wise <me@saschawise.com>
2024-09-26 09:40:32 -0700

    fix(s10): fix builds on windows with defaults (#726)

--- libs/s10/src/lib.rs
+++ libs/s10/src/lib.rs
@@ -1,12 +1,12 @@
 pub mod cli;
 pub mod error;
 pub mod recipe;
-#[cfg(feature = "nox-ecs")]
+#[cfg(not(target_os = "windows"))]
 pub mod sim;
 pub mod watch;
 
 pub use error::*;
 pub use recipe::*;
-#[cfg(feature = "nox-ecs")]
+#[cfg(not(target_os = "windows"))]
 pub use sim::*;
 pub use watch::*;
