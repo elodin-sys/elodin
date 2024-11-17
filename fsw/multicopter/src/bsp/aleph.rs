2df5eb23cb88e22f8173f94e743d30b3ee281734
Akhil Velagapudi <4@4khil.com>
2024-11-16 17:33:49 -0800

    fix(fsw): use correct HSE freq (#793)
    Also, stick to VOS1 frequency range for now (<= 400MHz). 480MHz requires
    some complex power register dance that we can figure out later.
    
    Signed-off-by: Akhil Velagapudi <4@4khil.com>

--- fsw/multicopter/src/bsp/aleph.rs
+++ fsw/multicopter/src/bsp/aleph.rs
@@ -10,32 +10,32 @@ pub fn clock_cfg(pwr: pac::PWR) -> clocks::Clocks {
     });
 
     let clock_cfg = clocks::Clocks {
-        pll_src: clocks::PllSrc::Hse(25_000_000), // 25 MHz
+        pll_src: clocks::PllSrc::Hse(16_000_000), // 16 MHz
         pll1: clocks::PllCfg {
             enabled: true,
             pllp_en: true,
             pllq_en: false,
             pllr_en: false,
-            divm: 5,   // pll_input_speed = 5 MHz
-            divn: 192, // vco_speed = 960 MHz
-            divp: 2,   // sysclk = 480 MHz
+            divm: 2,   // pll_input_speed = 8 MHz
+            divn: 100, // vco_speed = 800 MHz
+            divp: 2,   // sysclk = 400 MHz
             ..Default::default()
         },
-        input_src: clocks::InputSrc::Pll1, // 480 MHz (sysclk)
-        d1_core_prescaler: clocks::HclkPrescaler::Div1, // 480 MHz (M7 core)
-        hclk_prescaler: clocks::HclkPrescaler::Div2, // 240 MHz (hclk, M4 core)
-        d2_prescaler1: clocks::ApbPrescaler::Div2, // 120 MHz
-        d2_prescaler2: clocks::ApbPrescaler::Div2, // 120 MHz
-        d3_prescaler: clocks::ApbPrescaler::Div2, // 120 MHz
-        vos_range: clocks::VosRange::VOS0,
+        input_src: clocks::InputSrc::Pll1, // 400 MHz (sysclk)
+        d1_core_prescaler: clocks::HclkPrescaler::Div1, // 400 MHz (M7 core)
+        hclk_prescaler: clocks::HclkPrescaler::Div2, // 200 MHz (hclk, M4 core)
+        d2_prescaler1: clocks::ApbPrescaler::Div2, // 100 MHz
+        d2_prescaler2: clocks::ApbPrescaler::Div2, // 100 MHz
+        d3_prescaler: clocks::ApbPrescaler::Div2, // 100 MHz
+        vos_range: clocks::VosRange::VOS1,
         ..Default::default()
     };
 
-    assert_eq!(clock_cfg.sysclk(), 480_000_000);
-    assert_eq!(clock_cfg.d1cpreclk(), 480_000_000);
-    assert_eq!(clock_cfg.hclk(), 240_000_000);
-    assert_eq!(clock_cfg.apb1(), 120_000_000);
-    assert_eq!(clock_cfg.apb2(), 120_000_000);
+    assert_eq!(clock_cfg.sysclk(), 400_000_000);
+    assert_eq!(clock_cfg.d1cpreclk(), 400_000_000);
+    assert_eq!(clock_cfg.hclk(), 200_000_000);
+    assert_eq!(clock_cfg.apb1(), 100_000_000);
+    assert_eq!(clock_cfg.apb2(), 100_000_000);
 
     clock_cfg
 }
