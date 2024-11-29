504ad63b61fa877a07e339c0fd6ab359c876fc43
van mossavand <35608644+msvandad@users.noreply.github.com>
2024-11-29 15:22:22 -0800

    feat:(nox-py): egm gravity effector (#792)
    * feat(editor): WIP EGM08 model
    
    * feat(editor): WIP EGM08 model, added normalized coefficients
    
    * feat(editor): WIP EGM08 model, refactor for more efficient computation
    
    * feat(editor): working egm2008 with acceptable runtimes
    
    * feat(editor): working egm2008 moved to ../nox-py/python/elodin
    
    * feat(editor): egm08 integrated into cube sat
    
    * feat(editor): egm08 integrated into cube sat
    
    * feat(editor): ruff formatting
    
    * feat(editor): ruff formatting
    
    * feat(editor): changed pyproject.toml to ignore ambigous variable names
    
    * feat(editor): changed pyproject.toml to ignore ambigous variable names
    
    * feat(editor): renamed appname field of project directories
    
    * feat(editor): added cube sat browser compatibility
    
    Signed-off-by: Van Mossavand <van@elodin.systems>
    
    * feat(editor): ruff and clippy formatting
    
    Signed-off-by: Van Mossavand <van@elodin.systems>
    
    * feat(editor): fixed laoding files into cache directory
    
    Signed-off-by: Van Mossavand <van@elodin.systems>
    
    * feat(editor): working gravity models integrated into cube sat, j2 flag
    
    Signed-off-by: Van Mossavand <van@elodin.systems>
    
    * feat(editor): buildkite errors
    
    Signed-off-by: Van Mossavand <van@elodin.systems>
    
    * feat(editor): pr comments
    
    Signed-off-by: Van Mossavand <van@elodin.systems>
    
    * chore: add just recipe to encode videos (#791)
    
    Signed-off-by: Akhil Velagapudi <4@4khil.com>
    Signed-off-by: Van Mossavand <van@elodin.systems>
    
    * feat(docs): ball tutorial docs page (#790)
    
    * feat(docs): ball tutorial docs page
    
    * feat(docs): remove monte carlo reference
    
    Signed-off-by: Van Mossavand <van@elodin.systems>
    
    * fix(fsw): use correct HSE freq (#793)
    
    Also, stick to VOS1 frequency range for now (<= 400MHz). 480MHz requires
    some complex power register dance that we can figure out later.
    
    Signed-off-by: Akhil Velagapudi <4@4khil.com>
    Signed-off-by: Van Mossavand <van@elodin.systems>
    
    * feat(editor): minor imporvements
    
    ---------
    
    Signed-off-by: Van Mossavand <van@elodin.systems>
    Signed-off-by: Akhil Velagapudi <4@4khil.com>
    Co-authored-by: Van Mossavand <van@Vans-MBP.attlocal.net>
    Co-authored-by: Akhil Velagapudi <4@4khil.com>
    Co-authored-by: Daniel Driscoll <x46085@gmail.com>

---
+++ libs/nox-py/python/elodin/j2.py
@@ -0,0 +1,29 @@
+import numpy as np
+from numpy import linalg as la
+
+
+class J2:
+    def __init__(self):
+        self.r_ref = 6.378e6  # Earth's equatorial radius in meters
+        self.mu_earth = 3.986004418e14  # Earth's gravitational parameter in m^3/s^2
+        self.J2 = 1.08262668e-3
+
+    def compute_field(self, x, y, z, mass):
+        r = np.array([z, y, z])
+        m = mass
+        norm = la.norm(r)
+        e_r = r / norm
+        f = -self.mu_earth * m * r / (norm * norm * norm)
+        e_z = np.array([0.0, 0.0, 1.0])
+        j2 = (
+            -self.mu_earth
+            * m
+            * self.J2
+            * self.r_ref**2
+            * (
+                3 * z / (norm**5) * e_z
+                + (3.0 / (2.0 * norm**4) - 15.0 * z**2 / (2.0 * norm**6.0)) * e_r
+            )
+        )
+        field = f + j2
+        return np.array(field)
