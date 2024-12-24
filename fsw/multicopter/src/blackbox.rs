a7964219eefbf5725e9334f110cfd0ca3b08981a
Akhil Velagapudi <4@4khil.com>
2024-12-24 11:45:07 -0800

    feat(fsw): log CAN frames to SD card
    Signed-off-by: Akhil Velagapudi <4@4khil.com>

--- fsw/multicopter/src/blackbox.rs
+++ fsw/multicopter/src/blackbox.rs
@@ -12,6 +12,7 @@ use alloc::boxed::Box;
 use hal::gpio::Pin;
 use zerocopy::IntoBytes;
 
+use crate::dronecan;
 use crate::rtc::FakeTime;
 use crate::sdmmc;
 
@@ -29,10 +30,67 @@ pub struct Record {
 pub struct Blackbox<'a> {
     volume_mgr: embedded_sdmmc::VolumeManager<sdmmc::Sdmmc, FakeTime, 4, 4, 1>,
     fc_name: &'a str,
-    data_file: Option<embedded_sdmmc::RawFile>,
+    data_file: BufferedFile,
+    can_file: BufferedFile,
+    led: Pin,
+}
+
+pub struct BufferedFile {
+    file: Option<embedded_sdmmc::RawFile>,
     buf: Box<[u8]>,
     buf_len: usize,
-    led: Pin,
+}
+
+impl BufferedFile {
+    fn buf_remaining(&self) -> usize {
+        self.buf.len() - self.buf_len
+    }
+
+    fn buf_push(&mut self, data: &[u8]) {
+        let len = core::cmp::min(data.len(), self.buf_remaining());
+        self.buf[self.buf_len..self.buf_len + len].copy_from_slice(&data[..len]);
+        self.buf_len += len;
+    }
+
+    fn buf(&self) -> &[u8] {
+        &self.buf[..self.buf_len]
+    }
+
+    fn write(
+        &mut self,
+        data: &[u8],
+        volume_mgr: &mut embedded_sdmmc::VolumeManager<sdmmc::Sdmmc, FakeTime, 4, 4, 1>,
+        led: &mut Pin,
+    ) {
+        if data.len() > self.buf_remaining() {
+            self.flush(volume_mgr, led);
+        }
+        self.buf_push(data);
+    }
+
+    fn flush(
+        &mut self,
+        volume_mgr: &mut embedded_sdmmc::VolumeManager<sdmmc::Sdmmc, FakeTime, 4, 4, 1>,
+        led: &mut Pin,
+    ) {
+        if let Some(file) = self.file.as_mut() {
+            led.set_high();
+            defmt::trace!("Flushing buffered data to SD card");
+            let mut data_file = file.to_file(volume_mgr);
+            match data_file.write(self.buf()).and_then(|_| data_file.flush()) {
+                Ok(_) => {
+                    defmt::trace!("Flushed {} bytes to SD card", self.buf_len);
+                    self.file = Some(data_file.to_raw_file());
+                }
+                Err(err) => {
+                    defmt::error!("Failed to write to SD card: {}", Error::from(err));
+                    self.file = None;
+                }
+            }
+        }
+        led.set_low();
+        self.buf_len = 0;
+    }
 }
 
 #[derive(Debug, Copy, Clone, defmt::Format)]
@@ -60,10 +118,17 @@ impl<'a> Blackbox<'a> {
         Self {
             volume_mgr,
             fc_name,
-            data_file: None,
+            data_file: BufferedFile {
+                file: None,
+                buf: alloc::vec![0; 4 * 1024].into_boxed_slice(),
+                buf_len: 0,
+            },
+            can_file: BufferedFile {
+                file: None,
+                buf: alloc::vec![0; 1024].into_boxed_slice(),
+                buf_len: 0,
+            },
             led,
-            buf: alloc::vec![0; 4 * 1024].into_boxed_slice(),
-            buf_len: 0,
         }
     }
 
@@ -77,49 +142,51 @@ impl<'a> Blackbox<'a> {
         let mut dir = mkdir_if_not_exists(&mut root_dir, self.fc_name)?;
         let mut dir = mkdir_if_not_exists(&mut dir, "blackbox")?;
         let mut dir = mkdir_if_not_exists(&mut dir, dir_name)?;
-        let file =
+        let data_file =
             dir.open_file_in_dir("data.bin", embedded_sdmmc::Mode::ReadWriteCreateOrAppend)?;
         defmt::info!(
             "Opened /{}/blackbox/{}/data.bin for writing telemetry data",
             self.fc_name,
             dir_name
         );
-        self.data_file = Some(file.to_raw_file());
+        self.data_file.file = Some(data_file.to_raw_file());
+        let can_file =
+            dir.open_file_in_dir("can.bin", embedded_sdmmc::Mode::ReadWriteCreateOrAppend)?;
+        defmt::info!(
+            "Opened /{}/blackbox/{}/can.bin for writing CAN data",
+            self.fc_name,
+            dir_name
+        );
+        self.can_file.file = Some(can_file.to_raw_file());
         Ok(())
     }
 
     pub fn write_record(&mut self, record: Record) {
-        let record = record.as_bytes();
-        if record.len() > self.buf.len() - self.buf_len {
-            if let Err(err) = self.flush() {
-                defmt::error!("Failed to write to SD card: {}", Error::from(err));
-                self.data_file = None;
-            }
-        }
-        self.buf[self.buf_len..self.buf_len + record.len()].copy_from_slice(record);
-        self.buf_len += record.len();
+        self.data_file
+            .write(record.as_bytes(), &mut self.volume_mgr, &mut self.led)
     }
 
-    fn flush(&mut self) -> Result<(), Error> {
-        let to_flush = &self.buf[..self.buf_len];
-        self.buf_len = 0;
-        if let Some(data_file) = self.data_file.as_mut() {
-            self.led.set_high();
-            defmt::debug!("Flushing buffered data to SD card");
-            let mut data_file = data_file.to_file(&mut self.volume_mgr);
-            data_file.write(to_flush)?;
-            data_file.flush()?;
-            self.data_file = Some(data_file.to_raw_file());
-            self.led.set_low();
+    pub fn write_can(&mut self, msg: &dronecan::RawMessage) {
+        let id_bytes = msg.id.into_bytes();
+        let len = id_bytes.len() + msg.buf.len();
+        if (len + 4) > self.can_file.buf_remaining() {
+            self.can_file.flush(&mut self.volume_mgr, &mut self.led);
         }
-        Ok(())
+        self.can_file.buf_push(&(len as u32).to_le_bytes());
+        self.can_file.buf_push(&id_bytes);
+        self.can_file.buf_push(msg.buf);
     }
 
     pub fn disarm(&mut self) -> Result<(), Error> {
-        if let Some(data_file) = self.data_file.take() {
-            let mut data_file = data_file.to_file(&mut self.volume_mgr);
-            data_file.flush()?;
+        self.data_file.flush(&mut self.volume_mgr, &mut self.led);
+        self.can_file.flush(&mut self.volume_mgr, &mut self.led);
+        if let Some(data_file) = self.data_file.file.take() {
+            let data_file = data_file.to_file(&mut self.volume_mgr);
             data_file.close()?;
+        }
+        if let Some(can_file) = self.can_file.file.take() {
+            let can_file = can_file.to_file(&mut self.volume_mgr);
+            can_file.close()?;
         }
         Ok(())
     }
