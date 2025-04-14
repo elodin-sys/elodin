7bb0c77b0cd7c4a72af168f47b11d0ead7dbaf4b
Akhil Velagapudi <akhilvelagapudi@gmail.com>
2025-04-14 12:10:26 -0700

    feat(os): add more cuda packages + podman + cudainfo (#940)
    * feat(os): add more cuda packages
    
    With these changes, I confirmed cuda, cudnn, tensorrrt, and onnx work
    now OOTB. I just ran the resnet50.onnx model using cuda + tensorrt. I
    also ran the cudnn-test.
    
    Signed-off-by: Akhil Velagapudi <4@4khil.com>
    
    * feat(os): add cuinfo script
    
    This script just does a bunch of basic system checks + prints out useful
    information.
    
    Signed-off-by: Akhil Velagapudi <4@4khil.com>
    
    ---------
    
    Signed-off-by: Akhil Velagapudi <4@4khil.com>

--- images/aleph/modules/aleph-dev.nix
+++ images/aleph/modules/aleph-dev.nix
@@ -1,14 +1,56 @@
 {pkgs, ...}: let
-  pythonPackages = p:
-    with p; [
+  onnxruntime-gpu-wheel = ps:
+    ps.buildPythonPackage {
+      pname = "onnxruntime-gpu";
+      version = "1.16.3";
+      format = "wheel";
+
+      src = pkgs.fetchurl {
+        url = "https://pypi.jetson-ai-lab.dev/jp5/cu114/+f/43e/f0cec5f026159/onnxruntime_gpu-1.16.3-cp311-cp311-linux_aarch64.whl";
+        sha256 = "43ef0cec5f026159306e69540138f457ecbc8eb0282d1f7166761e7fbc84288e";
+      };
+
+      propagatedBuildInputs = with ps; [
+        coloredlogs
+        flatbuffers
+        numpy
+        packaging
+        protobuf
+        sympy
+      ];
+    };
+  cudainfo = pkgs.writeScriptBin "cudainfo" (builtins.readFile ../scripts/cudainfo.py);
+  pythonPackages = ps:
+    with ps; [
       pipx
       pip
       virtualenv
       numpy
-      # opencv4
-      # onnxruntime
+      wheel
+      onnx
+      tqdm
+      matplotlib
+      (onnxruntime-gpu-wheel ps)
     ];
 in {
+  environment.variables = {
+    LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath [
+      pkgs.stdenv.cc.cc.lib
+      pkgs.nvidia-jetpack.cudaPackages.cudatoolkit
+      pkgs.nvidia-jetpack.cudaPackages.cudnn
+      pkgs.nvidia-jetpack.cudaPackages.tensorrt
+      pkgs.nvidia-jetpack.cudaPackages.vpi2
+      pkgs.nvidia-jetpack.l4t-cuda
+      pkgs.nvidia-jetpack.l4t-gstreamer
+      pkgs.nvidia-jetpack.l4t-multimedia
+      pkgs.nvidia-jetpack.l4t-camera
+    ];
+  };
+  virtualisation.podman = {
+    enable = true;
+    # TODO: replace with `hardware.nvidia-container-toolkit.enable` when it works (https://github.com/nixos/nixpkgs/issues/344729).
+    enableNvidia = true;
+  };
   environment.systemPackages = with pkgs; [
     libgpiod_1
     dfu-util
@@ -32,11 +74,16 @@ in {
     vim
     htop
     dtc
-    nvidia-jetpack.cudaPackages.cudatoolkit
-    nvidia-jetpack.cudaPackages.tensorrt
-    nvidia-jetpack.l4t-tools
-    nvidia-jetpack.l4t-gstreamer
-    (python3.withPackages pythonPackages)
+    wget
+    btop
+    dpkg
+    opencv
+    cudainfo
+    gst_all_1.gstreamer
+    gst_all_1.gst-plugins-base
+    nvidia-jetpack.samples.cuda-test
+    nvidia-jetpack.samples.cudnn-test
+    (python311.withPackages pythonPackages)
     (v4l-utils.override {withGUI = false;})
     (writeShellScriptBin "reset-mcu" (builtins.readFile ../scripts/reset-mcu.sh))
     (writeShellScriptBin "flash-mcu" (builtins.readFile ../scripts/flash-mcu.sh))
