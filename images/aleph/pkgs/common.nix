79052ada04c0a32b3e39afb8949f2c1ab4499ae4
Akhil Velagapudi <akhilvelagapudi@gmail.com>
2025-04-17 10:34:47 -0700

    Fix overzealous cache invalidation (#948)
    * chore: remove unused .dockerignore
    
    Signed-off-by: Akhil Velagapudi <4@4khil.com>
    
    * feat(os): bump default user open file limits
    
    Signed-off-by: Akhil Velagapudi <4@4khil.com>
    
    * fix(os): ignore sig checks when copying to Aleph
    
    Signed-off-by: Akhil Velagapudi <4@4khil.com>
    
    * feat(os): add lsof
    
    Signed-off-by: Akhil Velagapudi <4@4khil.com>
    
    * fix(os): fix overzealous cache invalidation when unrelated files are
    changed
    
    Signed-off-by: Akhil Velagapudi <4@4khil.com>
    
    * chore: move examples/msg-test under libs/roci
    
    It simplifies the nix builds. Also, I think it's more appropriate there
    as examples/ was originally for putting sims.
    
    Signed-off-by: Akhil Velagapudi <4@4khil.com>
    
    ---------
    
    Signed-off-by: Akhil Velagapudi <4@4khil.com>

---
+++ images/aleph/pkgs/common.nix
@@ -0,0 +1,33 @@
+{lib}: {
+  src = let
+    includeSrc = orig_path: type: let
+      path = toString orig_path;
+      base = baseNameOf path;
+      relPath = lib.removePrefix (toString ../../..) orig_path;
+      matchesPrefix = lib.any (prefix: lib.hasPrefix prefix relPath) [
+        "/apps"
+        "/libs"
+        "/fsw"
+        "/.config"
+      ];
+      matchesSuffix = lib.any (suffix: lib.hasSuffix suffix base) [
+        "Cargo.toml"
+        "Cargo.lock"
+        ".rs"
+        ".c"
+        ".h"
+        ".cpp"
+        ".hpp"
+        ".jinja"
+      ];
+      matchesExclude = lib.any (exclude: lib.hasPrefix exclude relPath) [
+        "/libs/basilisk-rs/vendor"
+      ];
+    in
+      (type == "directory" && matchesPrefix && !matchesExclude) || matchesSuffix;
+  in
+    lib.cleanSourceWith {
+      src = ../../..;
+      filter = path: type: includeSrc path type;
+    };
+}
--- images/aleph/pkgs/mekf.nix
+++ images/aleph/pkgs/mekf.nix
@@ -6,20 +6,17 @@
 }: let
   rustToolchain = p: p.rust-bin.stable."1.85.0".default;
   craneLib = (crane.mkLib pkgs).overrideToolchain rustToolchain;
-  crateName = craneLib.crateNameFromCargoToml {cargoToml = ../../../fsw/mekf/Cargo.toml;};
-  regexesSrcFilter = regexes: path: _type: lib.any (re: builtins.match re path != null) regexes;
-  filterSrcRegexes = regexes: src:
-    lib.cleanSourceWith {
-      inherit src;
-      filter = path: type: (regexesSrcFilter regexes path type) || (craneLib.filterCargoSources path type);
-    };
-  src = filterSrcRegexes [".*toml$" ".*rs$" ".*jinja$" ".*h$" ".*hpp$" ".*c$" ".*cpp$"] ../../../.;
+  pname = (craneLib.crateNameFromCargoToml {cargoToml = ../../../fsw/mekf/Cargo.toml;}).pname;
+  version = (craneLib.crateNameFromCargoToml {cargoToml = ../../../Cargo.toml;}).version;
+
+  common = import ./common.nix {inherit lib;};
+  src = common.src;
+
   commonArgs = {
-    inherit (crateName) pname;
+    inherit pname version;
     inherit src;
-    version = "0.12.0";
     doCheck = false;
-    cargoExtraArgs = "--package=${crateName.pname}";
+    cargoExtraArgs = "--package=${pname}";
     HOST_CC = "${pkgs.stdenv.cc.nativePrefix}cc";
     TARGET_CC = "${pkgs.stdenv.cc.targetPrefix}cc";
     buildInputs = [
