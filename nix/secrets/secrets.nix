162b60ffd2dfc0fa6ffbdbd695107273a2350d84
Akhil Velagapudi <4@4khil.com>
2025-05-06 16:46:47 -0700

    chore: update ssh key for docs host (#50)
    Signed-off-by: akhilles <akhilvelagapudi@gmail.com>

--- nix/secrets/secrets.nix
+++ nix/secrets/secrets.nix
@@ -1,6 +1,7 @@
 let
-  aleph-7427 = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIN8prWlP7DynQYV/YZRnW7IxoIHYEHhH2/EgMBSTZ776";
+  aleph-7427 = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBqXJooZYJIyxkpvpnHMaKNmWVE3iqNZnAKUsVoRKkCR";
+  akhilles = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOh7/655LhcognTvqjkXNuP7HIS27IVeMhJUfp9oRd+o";
 in {
-  "tunnel-creds.age".publicKeys = [aleph-7427];
-  "tunnel-cert.age".publicKeys = [aleph-7427];
+  "tunnel-creds.age".publicKeys = [aleph-7427 akhilles];
+  "tunnel-cert.age".publicKeys = [aleph-7427 akhilles];
 }
