e2c93b0fb086abc2e6c2f20ac60f3a2ec3b23c8a
Shane Celis <shane.celis@gmail.com>
2025-09-02 13:48:40 -0400

    feature(editor): Log error on schematic load failure.

--- libs/elodin-editor/src/ui/schematic/load.rs
+++ libs/elodin-editor/src/ui/schematic/load.rs
@@ -54,7 +54,9 @@ pub fn sync_schematic(
     if let Some(path) = config.schematic_path() {
         let path = Path::new(path);
         if path.exists() {
-            if load_schematic_file(&path, params, live_reload_rx).is_ok() {
+            if let Err(e) = load_schematic_file(&path, params, live_reload_rx) {
+                bevy::log::error!(?e, "invalid schematic for {path:?}");
+            } else {
                 return;
             }
         }
