dbc375e3a2b80792b6806509012525b9cbd614ca
Shane Celis <shane.celis@gmail.com>
2025-09-16 08:37:53 -0700

    style: Reformat.

--- libs/impeller2/kdl/src/ser.rs
+++ libs/impeller2/kdl/src/ser.rs
@@ -734,15 +734,18 @@ object_3d "a.world_pos" {
         let parsed = parse_schematic(original_kdl).unwrap();
         let serialized = serialize_schematic(&parsed);
         // NOTE: fov and grid are dropped because they are the default value.
+        //
         //viewport active=#true hdr=#true fov=45.0 show_grid=#false
-        assert_eq!(r#"
+        assert_eq!(
+            r#"
 tabs {
     viewport active=#true hdr=#true
     graph a.world_pos name="a world_pos"
 }
 object_3d a.world_pos {
     sphere radius=0.20000000298023224 r=1.0 g=1.0 b=1.0
-}"#.trim(),
+}"#
+            .trim(),
             serialized
         );
         let reparsed = parse_schematic(&serialized).unwrap();
@@ -756,10 +759,7 @@ object_3d a.world_pos {
         let original_kdl = r#"graph "rocket.fin_deflect[0]" name=Fin "#;
         let parsed = parse_schematic(original_kdl).unwrap();
         let serialized = serialize_schematic(&parsed);
-        assert_eq!(
-            r#"graph "rocket.fin_deflect[0]" name=Fin"#,
-            serialized
-        );
+        assert_eq!(r#"graph "rocket.fin_deflect[0]" name=Fin"#, serialized);
         let reparsed = parse_schematic(&serialized).unwrap();
         assert_eq!(parsed.elems.len(), reparsed.elems.len());
     }
