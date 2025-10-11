2f47b81c0e47b90424a57d50e833a51515a1b345
x46085@gmail.com <x46085@gmail.com>
2025-10-11 14:33:53 -0700

    Consolidate into a unified elodin nix shell

--- nix/shell.nix
+++ nix/shell.nix
@@ -8,53 +8,13 @@ with pkgs; let
   xla_ext = pkgs.callPackage ./pkgs/xla-ext.nix {system = pkgs.stdenv.hostPlatform.system;};
   llvm = llvmPackages_latest;
 
+  # Import shared JAX overrides
+  jaxOverrides = pkgs.callPackage ./pkgs/jax-overrides.nix {inherit pkgs;};
+  
   # Create a Python environment with the same JAX version as our pyproject.toml
   pythonWithJax = let
     python3' = python3.override {
-      packageOverrides = self: super: {
-        # Override jaxlib to use version 0.4.31
-        jaxlib = super.jaxlib-bin.overridePythonAttrs (old: rec {
-          version = "0.4.31";
-          src = let
-            system = pkgs.stdenv.system;
-            platform =
-              if system == "x86_64-linux"
-              then "manylinux2014_x86_64"
-              else if system == "aarch64-linux"
-              then "manylinux2014_aarch64"
-              else "macosx_11_0_arm64";
-            wheelName = "jaxlib-${version}-cp312-cp312-${platform}.whl";
-            baseUrl = "https://files.pythonhosted.org/packages";
-            wheelUrls = {
-              "manylinux2014_x86_64" = "${baseUrl}/b1/09/58d35465d48c8bee1d9a4e7a3c5db2edaabfc7ac94f4576c9f8c51b83e70/${wheelName}";
-              "manylinux2014_aarch64" = "${baseUrl}/e0/af/10b49f8de2acc7abc871478823579d7241be52ca0d6bb0d2b2c476cc1b68/${wheelName}";
-              "macosx_11_0_arm64" = "${baseUrl}/68/cf/28895a4a89d88d18592507d7a35218b6bb2d8bced13615065c9f925f2ae1/${wheelName}";
-            };
-          in
-            pkgs.fetchurl {
-              url = wheelUrls.${platform} or (throw "Unsupported platform: ${platform}");
-              hash =
-                if system == "x86_64-linux"
-                then "sha256-Hxr6X9WKYPZ/DKWG4mcUrs5i6qLIM0wk0OgoWvxKfM0="
-                else if system == "aarch64-linux"
-                then "sha256-TYZ6GgVlsxz9qrvsgeAwLGRhuyrEuSwEZwMo15WBmAM="
-                else "sha256-yficGFKH5A7oFzpxQtZJUxHncs0TmpPcqT8NmcGHKDI="; # macosx_11_0_arm64
-            };
-        });
-        jaxlib-bin = self.jaxlib;
-
-        # Override JAX to use version 0.4.31
-        jax = super.jax.overridePythonAttrs (old: rec {
-          version = "0.4.31";
-          src = super.fetchPypi {
-            inherit (old) pname;
-            inherit version;
-            hash = "sha256-/S1HBkOgBz2CJzfweI9xORZWr35izFsueZXuOQzqwoc=";
-          };
-          pythonImportsCheck = [];
-          doCheck = false;
-        });
-      };
+      packageOverrides = jaxOverrides;
     };
   in
     python3'.withPackages (ps:
