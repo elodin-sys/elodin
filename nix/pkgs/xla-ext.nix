e6609b542802064c21a4a22a803a21d736c1853c
Joel Reymont <18791+joelreymont@users.noreply.github.com>
2025-10-13 20:57:31 +0300

    Rebase on top of the Rust 1.9 PR

--- nix/pkgs/xla-ext.nix
+++ nix/pkgs/xla-ext.nix
@@ -4,21 +4,21 @@
   lib,
   ...
 }: let
-  version = "0.5.4";
+  version = "0.9.1";
   suffix_map = {
     "aarch64-darwin" = "cpu.tar.gz";
     "aarch64-linux" = "gnu-cpu.tar.gz";
     "x86_64-linux" = "gnu-cpu.tar.gz";
   };
   sha256_map = {
-    "aarch64-darwin" = "sha256:0ykfnp6d78vp2yrhmr8wa3rlv6cri6mdl0fg034za839j7i7xqkz";
-    "aarch64-linux" = "sha256:0sy53r6qhw0n3n342s013nq5rnzlg1qdbmgpvawh3p35a21qy8xr";
-    "x86_64-linux" = "sha256:103mybbnz6fm2i3r0fy0nf23ffdjxb37wd4pzvmwn0dpczr6dkw1";
+    "aarch64-darwin" = "sha256:0m82waljhscajsdkpyd16c2spn1avfpp9a9am9nahlafa6lvrvrz";
+    "aarch64-linux" = lib.fakeSha256;
+    "x86_64-linux" = lib.fakeSha256;
   };
   suffix = builtins.getAttr system suffix_map;
-  filename = "xla_extension-${system}-${suffix}";
+  filename = "xla_extension-${version}-${system}-${suffix}";
   tarball = fetchTarball {
-    url = "https://github.com/elodin-sys/xla/releases/download/v${version}/${filename}";
+    url = "https://github.com/elodin-sys/xla-next/releases/download/v${version}/${filename}";
     sha256 = builtins.getAttr system sha256_map;
   };
 in
