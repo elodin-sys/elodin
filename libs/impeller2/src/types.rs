dee197df9268c04a9a14c1245836e29fcc4d6ab2
Shane Celis <shane.celis@gmail.com>
2025-11-04 18:35:47 -0800

    feat: Provide packet ID on no vtable error.

--- libs/impeller2/src/types.rs
+++ libs/impeller2/src/types.rs
@@ -805,7 +805,9 @@ impl<B: IoBuf> OwnedTable<B> {
         registry: &impl crate::registry::VTableRegistry,
         sink: &mut D,
     ) -> Result<Result<(), D::Error>, Error> {
-        let vtable = registry.get(&self.id).ok_or(Error::VTableNotFound)?;
+        let vtable = registry
+            .get(&self.id)
+            .ok_or_else(|| Error::VTableNotFound(self.id))?;
         vtable.apply(stellarator_buf::deref(&self.buf), sink)
     }
 }
