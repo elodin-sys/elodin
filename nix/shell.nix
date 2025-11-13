1b7117ab5d85b9262fb6075a5b633056a0cf16b2
zxq82lm <231263183+zxq82lm@users.noreply.github.com>
2025-11-13 15:22:51 -0800

    env DISPLAY:=0 in the nixhook

--- nix/shell.nix
+++ nix/shell.nix
@@ -155,6 +155,10 @@ in {
       doCheck = false;
 
       shellHook = ''
+        case "$(uname -s)" in
+          Linux*) export DISPLAY=:0 ;;
+        esac
+
         # start the shell if we're in an interactive shell
         if [[ $- == *i* ]]; then
           echo "━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━"
