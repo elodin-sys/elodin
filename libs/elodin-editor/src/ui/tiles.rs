1d1c42139e18fe6d5b425b64ca8b55b1d38741f2
zxq82lm <231263183+zxq82lm@users.noreply.github.com>
2026-01-22 18:56:18 -0800

    fix/extra-tab-linux (#434)
    * fix/extra-tab-linux

--- libs/elodin-editor/src/ui/tiles.rs
+++ libs/elodin-editor/src/ui/tiles.rs
@@ -1596,6 +1596,13 @@ impl egui_tiles::Behavior<Pane> for TreeBehavior<'_> {
                 t.clone()
             } else {
                 match tiles.get(tile_id) {
+                    Some(egui_tiles::Tile::Container(Container::Tabs(_))) => {
+                        // Hide Tabs containers without custom name (wrapper Tabs)
+                        let min_width = self.tab_title_spacing(ui.visuals()) * 2.0;
+                        let (_, rect) = ui.allocate_space(vec2(min_width, ui.available_height()));
+                        let response = ui.interact(rect, id, egui::Sense::click_and_drag());
+                        return self.on_tab_button(tiles, tile_id, response);
+                    }
                     Some(egui_tiles::Tile::Container(c)) => format!("{:?}", c.kind()),
                     _ => "Container".to_owned(),
                 }
