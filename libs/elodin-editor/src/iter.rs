ea4810d0655bd32b3d17d094874ab28272760f57
Shane Celis <scelis@singularityus.com>
2026-02-02 14:02:51 -0800

    chore: Don't use fmt::from_fn() for Rust 1.93.

--- libs/elodin-editor/src/iter.rs
+++ libs/elodin-editor/src/iter.rs
@@ -1,22 +1,36 @@
 use std::fmt;
 
+/// A wrapper that joins iterator items with a separator for Display formatting
+pub struct JoinDisplay<I> {
+    iter: I,
+    sep: &'static str,
+}
+
+impl<I> fmt::Display for JoinDisplay<I>
+where
+    I: Iterator + Clone,
+    I::Item: fmt::Display,
+{
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        let mut it = self.iter.clone();
+        if let Some(first) = it.next() {
+            write!(f, "{first}")?;
+            for item in it {
+                write!(f, "{}{}", self.sep, item)?;
+            }
+        }
+        Ok(())
+    }
+}
+
 /// Extension trait: `iter.join_display(", ")`
 pub trait JoinDisplayExt: Iterator + Sized {
-    fn join_display(self, sep: &'static str) -> impl fmt::Display
+    fn join_display(self, sep: &'static str) -> JoinDisplay<Self>
     where
         Self: Clone,
         Self::Item: fmt::Display,
     {
-        fmt::from_fn(move |f| {
-            let mut it = self.clone();
-            if let Some(first) = it.next() {
-                write!(f, "{first}")?;
-                for item in it {
-                    write!(f, "{sep}{item}")?;
-                }
-            }
-            Ok(())
-        })
+        JoinDisplay { iter: self, sep }
     }
 }
 
