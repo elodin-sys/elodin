e8e1328c60d9b7c6a93a0af2180cddde07dc20c5
Shane Celis <shane.celis@gmail.com>
2026-03-11 17:08:36 -0400

    style: Reformat.

--- apps/elodin/build.rs
+++ apps/elodin/build.rs
@@ -7,7 +7,9 @@ fn main() {
             .ok()
             .and_then(|o| {
                 if o.status.success() {
-                    String::from_utf8(o.stdout).ok().map(|s| s.trim().to_string())
+                    String::from_utf8(o.stdout)
+                        .ok()
+                        .map(|s| s.trim().to_string())
                 } else {
                     None
                 }
