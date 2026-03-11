1b2477c15c13a3c4baa1228dd5234de475250200
Shane Celis <shane.celis@gmail.com>
2026-03-11 16:08:36 -0400

    chore: Make git hash work inside nix too.

--- apps/elodin/build.rs
+++ apps/elodin/build.rs
@@ -1,38 +1,31 @@
 fn main() {
-    let mut hash = std::process::Command::new("git")
-        .args(["rev-parse", "--short", "HEAD"])
-        .output()
-        .ok()
-        .and_then(|o| {
-            if o.status.success() {
-                String::from_utf8(o.stdout).ok()
-            } else {
-                None
-            }
-        })
-        .map(|s| s.trim().to_string())
-        .unwrap_or_else(|| "unknown".to_string());
+    let mut hash = match std::env::var("GIT_HASH") {
+        Ok(s) if !s.is_empty() => s,
+        _ => std::process::Command::new("git")
+            .args(["rev-parse", "--short", "HEAD"])
+            .output()
+            .ok()
+            .and_then(|o| {
+                if o.status.success() {
+                    String::from_utf8(o.stdout).ok().map(|s| s.trim().to_string())
+                } else {
+                    None
+                }
+            })
+            .unwrap_or_else(|| "unknown".to_string()),
+    };
 
-    let is_dirty = std::process::Command::new("git")
-        .args(["diff", "--quiet", "--exit-code", "HEAD"])
-        .status()
-        .map(|status| {
-            if status.success() {
-                false
-            } else {
-                true
-            }
-        });
-    match is_dirty {
-        Ok(is_dirty) => {
-            if is_dirty {
+    if std::env::var("GIT_HASH").is_err() {
+        if let Ok(status) = std::process::Command::new("git")
+            .args(["diff", "--quiet", "--exit-code", "HEAD"])
+            .status()
+        {
+            if !status.success() {
                 hash.push('*');
             }
         }
-        Err(e) => {
-            eprintln!("error: Failed to get git hash {e}");
-        }
     }
+
     println!("cargo:rustc-env=GIT_HASH={}", hash);
     println!("cargo:rerun-if-changed=../../.git/HEAD");
 }
