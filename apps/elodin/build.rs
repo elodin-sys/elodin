732cd2fe1dfbac2c8dadb9508f54d11619863f48
Shane Celis <shane.celis@gmail.com>
2026-03-12 00:54:02 -0400

    refactor: Extract build-common/git_inspect.rs.

--- apps/elodin/build.rs
+++ apps/elodin/build.rs
@@ -1,33 +1,12 @@
+#[path = "../../libs/build-common/git_inspect.rs"]
+mod git_inspect;
 fn main() {
-    let mut hash = match std::env::var("GIT_HASH") {
-        Ok(s) if !s.is_empty() => Some(s),
-        _ => std::process::Command::new("git")
-            .args(["rev-parse", "--short", "HEAD"])
-            .output()
-            .ok()
-            .and_then(|o| {
-                if o.status.success() {
-                    String::from_utf8(o.stdout)
-                        .ok()
-                        .map(|s| s.trim().to_string())
-                } else {
-                    None
-                }
-            }),
-    };
-
-    if let Some(ref mut hash) = hash
-        && let Ok(status) = std::process::Command::new("git")
-            .args(["diff", "--quiet", "--exit-code", "HEAD"])
-            .status()
-        && !status.success()
-    {
-        hash.push('*');
-    }
-
+    let hash = git_inspect::short_hash();
     println!(
         "cargo:rustc-env=GIT_HASH={}",
         hash.unwrap_or_else(|| "unknown".to_string())
     );
-    println!("cargo:rerun-if-changed=../../.git/HEAD");
+    if let Some(git_head_path) = git_inspect::head_path() {
+        println!("cargo:rerun-if-changed={}", &git_head_path);
+    }
 }
