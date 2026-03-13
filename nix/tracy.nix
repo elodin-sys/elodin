05f2096591113b96c823fabceac08ebd8d20c67a
zxq82lm <231263183+zxq82lm@users.noreply.github.com>
2026-03-12 17:20:55 -0700

    fix(nix): set GLIBC_TUNABLES as shell env instead of shellHook.

--- nix/tracy.nix
+++ nix/tracy.nix
@@ -1,6 +1,7 @@
 {pkgs}:
 pkgs.mkShell {
   name = "elodin-tracy";
+  GLIBC_TUNABLES = "glibc.rtld.optional_static_tls=4096";
   packages =
     [
       pkgs.elodin.elodin-py-tracy.py
@@ -15,7 +16,4 @@ pkgs.mkShell {
     ++ pkgs.lib.optionals pkgs.stdenv.isLinux [
       pkgs.elodin.iree_runtime_tracy
     ];
-  shellHook = ''
-    export GLIBC_TUNABLES=glibc.rtld.optional_static_tls=4096
-  '';
 }
